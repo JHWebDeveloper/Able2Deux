6ec1be185d9029712f61d52e7e46c9608a23f833
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-06-10 02:12:16 -0400

    removed return and brackets

--- src/renderer/components/render/selector/EditAll.js
+++ src/renderer/components/render/selector/EditAll.js
@@ -5,18 +5,16 @@ import { toggleCheckbox } from '../../../actions'
 
 import Checkbox from '../../form_elements/Checkbox'
 
-const EditAll = ({ editAll, dispatch }) => {
-	return (
-		<div>
-			<Checkbox
-				label="Edit All"
-				name="editAll"
-				checked={editAll}
-				onChange={e => dispatch(toggleCheckbox(e))}
-				switchIcon />
-		</div>
-	)
-}
+const EditAll = ({ editAll, dispatch }) => (
+	<div>
+		<Checkbox
+			label="Edit All"
+			name="editAll"
+			checked={editAll}
+			onChange={e => dispatch(toggleCheckbox(e))}
+			switchIcon />
+	</div>
+)
 
 EditAll.propTypes = {
 	editAll: bool.isRequired,
