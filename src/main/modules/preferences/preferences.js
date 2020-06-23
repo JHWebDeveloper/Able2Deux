07cca2494e4010eee05cddd7eb9e17faae43288e
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-06-23 16:42:56 -0400

    fixed error. version prop wasnt included when saving prefs json

--- src/main/modules/preferences/preferences.js
+++ src/main/modules/preferences/preferences.js
@@ -1,6 +1,12 @@
 import { promises as fsp } from 'fs'
 import { prefsPath } from '../utilities/extFileHandlers'
+import defaultPrefs from './default'
 
 export const loadPrefs = async () => JSON.parse(await fsp.readFile(prefsPath))
 
-export const savePrefs = async prefs => fsp.writeFile(prefsPath, JSON.stringify(prefs))
+export const savePrefs = async prefs => (
+	fsp.writeFile(prefsPath, JSON.stringify({
+		...prefs,
+		version: defaultPrefs.version
+	}))
+)
