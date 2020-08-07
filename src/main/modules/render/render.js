053cdd0a3fe236787bea2c90b7f43c71dc3a8759
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-07 01:26:04 -0400

    added export settings for audio conversion and extraction

--- src/main/modules/render/render.js
+++ src/main/modules/render/render.js
@@ -23,32 +23,54 @@ const removeJob = async id => {
 	return temp.exports.clear(id)
 }
 
+const sharedVideoOptions = (renderFrameRate) => [
+	...renderFrameRate === 'auto' ? [] : ['-r 59.94'],
+	'-b:v 7000k',
+	'-preset:v ultrafast',
+	'-c:a aac',
+	'-b:a 192k',
+	'-ar 48000'
+]
+
 export const render = (exportData, win) => new Promise((resolve, reject) => {
-	const { id, arc, background, overlay, sourceData, rotation, renderOutput, renderFrameRate, saveLocations, start, end } = exportData
+	const { mediaType, id, audio, arc, background, overlay, sourceData, rotation, renderOutput, renderFrameRate, saveLocations, start, end } = exportData
 	const [ renderWidth, renderHeight ] = renderOutput.split('x')
 
-	const needsAlpha = background === 'alpha' && (arc === 'fit' || arc === 'transform')
-	const extension  = needsAlpha ? 'mov' : 'mp4'
-	const exportPath = path.join(temp.exports.path, `${id}.${extension}`)
-	const saveName = `${exportData.filename}.${extension}`
+	const isAudio = mediaType === 'audio' || mediaType === 'video' && audio.exportAs === 'audio'
+	const needsAlpha = !isAudio && background === 'alpha' && (arc === 'fit' || arc === 'transform')
+	let outputOptions = []
+	let extension = false
 	let backgroundFile = false
 	let overlayDim = false
 
-	const command = ffmpeg(exportData.tempFilePath)
-		.outputOptions(needsAlpha ? [
-			'-vcodec prores_ks',
-			...renderFrameRate === 'auto' ? [] : ['-r 59.94'],
+	if (isAudio && audio.format === 'file') {
+		outputOptions = ['-c:a pcm_s24le']
+
+		extension = 'wav'
+	} else if (needsAlpha) {
+		outputOptions = [
+			'-c:v prores_ks',
 			'-pix_fmt yuva444p10le',
 			'-profile:v 4444',
-			'-preset:v ultrafast'
-		] : [
-			'-vcodec h264',
-			...renderFrameRate === 'auto' ? [] : ['-r 59.94'],
-			'-b:v 7000k',
-			'-b:a 192k',
+			...sharedVideoOptions(renderFrameRate)
+		]
+
+		extension = 'mov'
+	} else {
+		outputOptions = [
+			'-c:v libx264',
 			'-crf 17',
-			'-preset:v ultrafast'
-		])
+			...sharedVideoOptions(renderFrameRate)
+		]
+
+		extension = 'mp4'
+	}
+
+	const exportPath = path.join(temp.exports.path, `${id}.${extension}`)
+	const saveName = `${exportData.filename}.${extension}`
+
+	const command = ffmpeg(exportData.tempFilePath)
+		.outputOptions(outputOptions)
 		.output(exportPath)
 		.on('progress', progress => {
 			win.webContents.send(`renderProgress_${id}`, {
@@ -81,54 +103,77 @@ export const render = (exportData, win) => new Promise((resolve, reject) => {
 				reject(err)
 			}
 		})
+	
+	if (start.enabled && start.tc >= exportData.duration) {
+		reject(new Error(`Bad inputs for ${exportData.filename}: start timecode exceeds duration.`))
+	}
+
+	if (start.enabled && end.enabled && end.tc <= start.tc) {
+		reject(new Error(`Bad inputs for ${exportData.filename}: end timecode preceeds start timecode.`))
+	}
 
 	if (start.enabled) command.seekInput(start.tc)
 	if (end.enabled) command.duration(start.enabled ? end.tc - start.tc : end.tc)
-	if (exportData.mediaType === 'image') command.loop(7)
 
-	if (sourceData) {
-		const sourcePng = path.join(temp.imports.path, `${id}.src-overlay.png`)
-		fs.writeFileSync(sourcePng, sourceData, 'base64')		
-		command.input(sourcePng)
-	}
+	if (!isAudio) {
+		if (mediaType === 'video' && audio.exportAs === 'video') command.noAudio()
+		if (mediaType === 'image') command.loop(7)
+	
+		if (sourceData) {
+			const sourcePng = path.join(temp.imports.path, `${id}.src-overlay.png`)
+			fs.writeFileSync(sourcePng, sourceData, 'base64')		
+			command.input(sourcePng)
+		}
+	
+		if (arc !== 'none' && !(arc === 'fill' && overlay === 'none')) {
+			backgroundFile = path.join(assetsPath, renderHeight, `${background}.${background === 'blue' || background === 'grey' ? 'mov' : 'png'}`)
+		}
+	
+		if (arc !== 'none' && overlay !== 'none') {
+			const overlayPng = path.join(assetsPath, renderHeight, `${overlay}.png`)
+	
+			command.input(backgroundFile).input(overlayPng)
+			
+			backgroundFile = path.join(assetsPath, renderHeight, 'black.png')
+			overlayDim = getOverlayInnerDimensions(renderHeight, overlay)
+		}
+	
+		const filterData = {
+			...rotation,
+			renderHeight,
+			renderWidth,
+			overlayDim,
+			sourceData: !!sourceData
+		}
+	
+		if (arc === 'none') {
+			filter.none(command, filterData)
+		} else if (arc === 'fill') {
+			filter.fill(command, {
+				...filterData,
+				centering: exportData.centering
+			})
+		} else if (arc === 'fit') {
+			filter.fit(command, backgroundFile, filterData)
+		} else if (arc === 'transform') {
+			filter.transform(command, backgroundFile, {
+				...filterData,
+				position: exportData.position,
+				scale: exportData.scale,
+				crop: exportData.crop
+			})
+		}
+	} else if (audio.format === 'bars') {
+		command
+			.input(path.join(assetsPath, renderHeight, 'bars.png'))
+			.loop()
+			.outputOptions([
+				'-r 59.94',
+				'-b:v 1k',
+				'-shortest'
+			])
 
-	if (arc !== 'none' && !(arc === 'fill' && overlay === 'none')) {
-		backgroundFile = path.join(assetsPath, renderHeight, `${background}.${background === 'blue' || background === 'grey' ? 'mov' : 'png'}`)
-	}
-
-	if (arc !== 'none' && overlay !== 'none') {
-		const overlayPng = path.join(assetsPath, renderHeight, `${overlay}.png`)
-
-		command.input(backgroundFile).input(overlayPng)
-		
-		backgroundFile = path.join(assetsPath, renderHeight, 'black.png')
-		overlayDim = getOverlayInnerDimensions(renderHeight, overlay)
-	}
-
-	const filterData = {
-		...rotation,
-		renderHeight,
-		renderWidth,
-		overlayDim,
-		sourceData: !!sourceData
-	}
-
-	if (arc === 'none') {
-		filter.none(command, filterData)
-	} else if (arc === 'fill') {
-		filter.fill(command, {
-			...filterData,
-			centering: exportData.centering
-		})
-	} else if (arc === 'fit') {
-		filter.fit(command, backgroundFile, filterData)
-	} else if (arc === 'transform') {
-		filter.transform(command, backgroundFile, {
-			...filterData,
-			position: exportData.position,
-			scale: exportData.scale,
-			crop: exportData.crop
-		})
+		if (mediaType === 'video') filter.videoToBars(command, { renderWidth, renderHeight })
 	}
 	
 	command.run()
