be8282669caec266f75efef299240ef141b9f50a
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-07 01:22:33 -0400

    changed extension to webm

--- src/main/modules/aquisition/saveScreenRecording.js
+++ src/main/modules/aquisition/saveScreenRecording.js
@@ -17,7 +17,7 @@ const fixScreenRecord = (original, filePath) => new Promise((resolve, reject) =>
 
 export const saveScreenRecording = async ({ id, buffer }) => {
 	const filePath = path.join(temp.imports.path, `${id}.mp4`)
-	const original = path.join(temp.imports.path, `${id}.original.mp4`)
+	const original = path.join(temp.imports.path, `${id}.original.webm`)
 
 	await fsp.writeFile(original, buffer)
 	await fixScreenRecord(original, filePath)
