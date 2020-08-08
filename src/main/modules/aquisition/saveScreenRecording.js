6ed13f1decf47547cdd111ecee8278ec53116e6e
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-07 21:52:47 -0400

    removed fixScreenRecording, function no longer needed

--- src/main/modules/aquisition/saveScreenRecording.js
+++ src/main/modules/aquisition/saveScreenRecording.js
@@ -1,28 +1,11 @@
 import { promises as fsp } from 'fs'
 import path from 'path'
-import ffmpeg from '../utilities/ffmpeg'
 import { temp } from '../utilities/extFileHandlers'
 
-const fixScreenRecord = (original, filePath) => new Promise((resolve, reject) => {
-	ffmpeg(original)
-		.outputOptions([
-			'-filter:v fps=fps=60',
-			'-preset:v ultrafast'
-		])
-		.output(filePath)
-		.on('end', resolve)
-		.on('error', reject)
-		.run()
-})
-
 export const saveScreenRecording = async ({ id, buffer }) => {
-	const filePath = path.join(temp.imports.path, `${id}.mp4`)
-	const original = path.join(temp.imports.path, `${id}.original.webm`)
+	const filePath = path.join(temp.imports.path, `${id}.webm`)
 
-	await fsp.writeFile(original, buffer)
-	await fixScreenRecord(original, filePath)
-	
-	fsp.unlink(original)
+	await fsp.writeFile(filePath, buffer)
 
 	return filePath
 }
