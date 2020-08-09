3b9250653ddac0649d7fc704bc5a32f3b58199ad
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-09 00:42:16 -0400

    added scroll effect on hover for media titles that get cut off in info display

--- src/renderer/components/render/selector/MediaInfo.js
+++ src/renderer/components/render/selector/MediaInfo.js
@@ -3,10 +3,34 @@ import { number, string } from 'prop-types'
 
 import { compareProps, secondsToTC, zeroize } from '../../../utilities'
 
+let interval = false
+
+const scrollText = e => {
+	e.persist()
+
+	requestAnimationFrame(() => { // sync to framerate
+		interval = setInterval(() => {
+			if (e.target.scrollWidth <= e.target.clientWidth) {
+				return clearInterval(interval)
+			}
+	
+			e.target.innerText = e.target.innerText.slice(1)
+		}, 1000 / 6) // run every 10 frames
+	})
+}
+
+const resetText = e => {
+	clearInterval(interval)
+	e.target.innerText = e.target.dataset.title
+}
+
 const MediaInfo = memo(({ thumbnail, title, width, height, aspectRatio, duration, fps }) => (
 	<div>
 		<img src={thumbnail} alt={title} />
-		<h2 title={title}>{title}</h2>
+		<h2
+			data-title={title}
+			onMouseEnter={e => scrollText(e)}
+			onMouseLeave={e => resetText(e)}>{title}</h2>
 		<ul>
 			{!!duration && <li>{secondsToTC(duration)};{zeroize(Math.round(duration % 1 * fps))}</li>}
 			{!!width && !!height && <li>{width}x{height}</li>}
