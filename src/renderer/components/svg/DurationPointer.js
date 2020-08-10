e30381c97ab56605cf10c8b702094986565b1bfe
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-10 14:55:12 -0400

    first commit

---
+++ src/renderer/components/svg/DurationPointer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DurationPointer = () => (
+	<svg width="41px" height="70px" viewBox="0 0 41 70">
+		<path fill="#7c7c7c" d="M14.083,68.5c-0.276,0-0.5-0.224-0.5-0.5V37.098c0-1.408,1.146-2.554,2.555-2.554h19.945
+			c0.276,0,0.5,0.224,0.5,0.5s-0.224,0.5-0.5,0.5H16.138c-0.857,0-1.555,0.697-1.555,1.554V68C14.583,68.276,14.359,68.5,14.083,68.5z"/>
+	</svg>
+)
+
+export default DurationPointer
