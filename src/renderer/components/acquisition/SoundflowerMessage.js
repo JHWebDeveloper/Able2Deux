7192d453aacc79788233c0ab1c40dad2515dc2ab
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-11 02:59:45 -0400

    first commit

---
+++ src/renderer/components/acquisition/SoundflowerMessage.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+const { interop } = window.ABLE2
+
+const SoundflowerMessage = () => {
+	const [ show, setShow ] = useState(false)
+
+	useEffect(() => {
+		(async () => {
+			setShow(!(await interop.findSoundflower()))
+		})()
+	}, [])
+
+	return show ? (
+		<a title="Get Soundflower" onClick={interop.getSoundflower}>
+			(Soundflower is required for audio on Mac)
+		</a>
+	) : <></>
+}
+
+export default SoundflowerMessage
