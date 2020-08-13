a8d0397b21149584140fd5ce6cead213dee04bed
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-13 17:40:35 -0400

    merged preview and audio preview into one components and deleted audio preview

--- src/renderer/components/render/Render.js
+++ src/renderer/components/render/Render.js
@@ -12,7 +12,6 @@ import BatchName from './BatchName'
 import SaveOptions from './SaveOptions'
 import SaveButtons from './SaveButtons'
 import Preview from './preview/Preview'
-import AudioPreview from './preview/AudioPreview'
 import EditorOptions from './editor/EditorOptions'
 import RenderQueue from './render-queue/RenderQueue'
 
@@ -74,9 +73,9 @@ const Render = () => {
 				<SaveButtons setRendering={setRendering} />
 			</div>
 			<div id="editor">
-				{mediaType === 'audio' || mediaType === 'video' && selected.audio.exportAs === 'audio'
-					? <AudioPreview format={selected.audio.format}/>
-					: <Preview selected={selected} dispatch={dispatch} />}
+				<Preview
+					selected={selected}
+					dispatch={dispatch} />
 				<EditorOptions
 					batch={batch}
 					editAll={editAll}
