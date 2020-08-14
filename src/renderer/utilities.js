0fed690ce87b5d1150dc9bd72a93a902ad257ca8
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-14 19:26:32 -0400

    added ScrollpadAdder class

--- src/renderer/utilities.js
+++ src/renderer/utilities.js
@@ -176,6 +176,37 @@ export const keepInRange = e => {
 	return e
 }
 
+export class ScrollbarPadder {
+	constructor() {
+		this.el = false
+		this.pad = 0
+		this.observer = new MutationObserver(this.addPad)
+	}
+
+	addPad = () => {
+		this.el.style.paddingRight = this.el.scrollHeight > this.el.clientHeight
+			? `${this.pad}px`
+			: 0
+	}
+
+	observe = (el, pad) => {
+		this.el = el
+		this.pad = pad
+		
+		this.addPad()
+
+		this.observer.observe(el, {
+			attributes: true,
+			childList: true,
+			subtree: true
+		})
+	}
+
+	disconnect = () => {
+		this.observer.disconnect()
+	}
+}
+
 export const toastrOpts = {
 	closeButton: true,
 	positionClass: 'toast-bottom-right',
