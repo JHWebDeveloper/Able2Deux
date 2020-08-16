c4160124a420f2d59e4f0d5b68b66ce3ce6e04b1
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-15 22:45:28 -0400

    passed autoPNG to render action

--- src/renderer/components/render/render-queue/RenderQueue.js
+++ src/renderer/components/render/render-queue/RenderQueue.js
@@ -14,7 +14,7 @@ const { interop } = window.ABLE2
 
 const RenderQueue = withRouter(params => {
 	const { media, batch, saveLocations, closeRenderQueue, dispatch, history } = params
-	const { renderOutput, renderFrameRate, concurrent } = useContext(PrefsContext)
+	const { renderOutput, renderFrameRate, autoPNG, concurrent } = useContext(PrefsContext)
 	
 	const complete = media.every(({ render }) => (
 		render.status === STATUS.COMPLETE ||
@@ -51,6 +51,7 @@ const RenderQueue = withRouter(params => {
 			saveLocations,
 			renderOutput,
 			renderFrameRate,
+			autoPNG,
 			concurrent,
 			goBack
 		}))
