41c10f6e35d601a0cb585cbfd31799cf661b98bd
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-21 22:17:44 -0400

    moved Konami to constructors subdirectory

--- src/renderer/components/preferences/Preferences.js
+++ src/renderer/components/preferences/Preferences.js
@@ -4,43 +4,13 @@ import '../../css/preferences.css'
 
 import { PrefsProvider, PrefsContext } from '../../store/preferences'
 import { toastrOpts } from '../../utilities'
+import { Konami } from '../../constructors'
 
 import RenderOutput from './RenderOutput'
 import ScratchDisk from './ScratchDisk'
 import Defaults from './Defaults'
 import SaveLocations from './SaveLocations'
 import SaveAndClose from './SaveAndClose'
-
-class Konami {
-	constructor() {
-		this.keys = [38, 38, 40, 40, 37, 39, 37, 39, 65, 66, 13]
-		this.count = 0
-		this.callback = false
-	}
-
-	log = e => {
-		const match = e.keyCode === this.keys[this.count]
-
-		if (match && this.count === this.keys.length - 1) {
-			this.callback()
-			this.count = 0
-		} else if (match) {
-			this.count += 1
-		} else {
-			this.count = 0
-		}
-	}
-
-	listen = callback => {
-		this.callback = callback
-		window.addEventListener('keydown', this.log)
-	}
-
-	remove = () => {
-		this.callback = false
-		window.removeEventListener('keydown', this.log)
-	}
-}
 
 const konami = new Konami()
 
