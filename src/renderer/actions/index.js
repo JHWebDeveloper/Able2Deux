bf127e4484eb850168196714960f1ff88bcdc7cb
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-08-21 21:24:24 -0400

    corrected aggregate export syntax

--- src/renderer/actions/index.js
+++ src/renderer/actions/index.js
@@ -1,15 +1,6 @@
-import * as acquisition from './acquisition'
-import * as buildSource from './buildSource'
-import * as constructors from './constructors'
-import * as main from '.'
-import * as preferences from './preferences'
-import * as render from './render'
-
-export default {
-	...acquisition,
-	...buildSource,
-	...constructors,
-	...main,
-	...preferences,
-	...render
-}
+export * from './acquisition'
+export * from './buildSource'
+export * from './constructors'
+export * from './main'
+export * from './preferences'
+export * from './render'
