b7fb367a423da434b13fb7d59c52a8e02b2ba6c0
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-09-08 15:22:48 -0400

    disable filename field conditions now looks at isBatch. fixed bug where filenames in single item batches with a lingering batchName were remaining disabled

--- src/renderer/components/formatting/editor/FileOptions.js
+++ src/renderer/components/formatting/editor/FileOptions.js
@@ -33,7 +33,7 @@ const FileOptions = memo(props => {
 
 	return (
 		<DetailsWrapper summary="File" id="file" open>
-			<fieldset disabled={!!props.batch.name && props.batch.position === 'replace'}>
+			<fieldset disabled={props.isBatch && props.batch.name && props.batch.position === 'replace'}>
 				<legend>Filename:</legend>
 				<input
 					type="text"
