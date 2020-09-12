cddb6d7d083d380e011118a3fde3bd5cd943aa19
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-09-12 13:10:08 -0400

    added $r token to replaceTokens method

--- src/renderer/utilities/valueModifiers.js
+++ src/renderer/utilities/valueModifiers.js
@@ -42,6 +42,7 @@ export const replaceTokens = (filename, i = 0, l = 0) => {
 
 	return filename
 		.replace(/\$n/g, zeroize(i + 1, l))
+		.replace(/\$r/g, zeroize(l - (i + 1), l))
 		.replace(/\$s/g, l)
 		.replace(/\$d/g, d.toDateString())
 		.replace(/\$D/g, d.toLocaleDateString().replace(/\//g, '-'))
