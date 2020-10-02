cc2ed58f99c31c9a0f8797ffab9401ac17925410
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-02 15:44:50 -0400

    simplified arrow function by removing return

--- src/renderer/components/form_elements/SliderPair.js
+++ src/renderer/components/form_elements/SliderPair.js
@@ -4,25 +4,23 @@ import { arrayOf, bool, func, number, oneOf, oneOfType, shape, string } from 'pr
 import Slider from './Slider'
 import LinkIcon from '../svg/LinkIcon'
 
-const SliderPair = ({ link, linkAction, sliders, pairAction }) => {	
-	return (
-		<div className="slider-pair">
-			<Slider
-				{...sliders[0]}
-				onChange={link ? pairAction : sliders[0].onChange} />
-			<Slider
-				{...sliders[1]}
-				onChange={link ? pairAction : sliders[1].onChange} />
-			<button
-				type="button"
-				name="link"
-				title={`${link ? 'Unlink' : 'Link'} Values`}
-				onClick={linkAction}>
-				<LinkIcon linked={link}/>
-			</button>
-		</div>
-	)
-}
+const SliderPair = ({ link, linkAction, sliders, pairAction }) => (
+	<div className="slider-pair">
+		<Slider
+			{...sliders[0]}
+			onChange={link ? pairAction : sliders[0].onChange} />
+		<Slider
+			{...sliders[1]}
+			onChange={link ? pairAction : sliders[1].onChange} />
+		<button
+			type="button"
+			name="link"
+			title={`${link ? 'Unlink' : 'Link'} Values`}
+			onClick={linkAction}>
+			<LinkIcon linked={link}/>
+		</button>
+	</div>
+)
 
 SliderPair.propTypes = {
 	link: bool.isRequired,
