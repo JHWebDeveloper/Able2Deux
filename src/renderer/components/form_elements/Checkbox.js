e2e9159410ae7ad2c1aaaa91f50030aa2b7d52f6
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-02 15:40:15 -0400

    simplified arrow function by removing return

--- src/renderer/components/form_elements/Checkbox.js
+++ src/renderer/components/form_elements/Checkbox.js
@@ -3,21 +3,19 @@ import { bool, func, string } from 'prop-types'
 
 import ToggleSwitch from '../svg/ToggleSwitch'
 
-const Checkbox = ({ label, name, checked, switchIcon, disabled, onChange }) => {
-	return (
-		<label>
-			<input
-				type="checkbox"
-				name={name}
-				className={switchIcon ? 'switch' : ''}
-				checked={checked}
-				onChange={onChange}
-				disabled={disabled} />
-			{switchIcon && <ToggleSwitch toggle={checked} disabled={disabled} />}
-			{!!label && <span>{label}</span>}
-		</label>
-	)
-}
+const Checkbox = ({ label, name, checked, switchIcon, disabled, onChange }) => (
+	<label>
+		<input
+			type="checkbox"
+			name={name}
+			className={switchIcon ? 'switch' : ''}
+			checked={checked}
+			onChange={onChange}
+			disabled={disabled} />
+		{switchIcon && <ToggleSwitch toggle={checked} disabled={disabled} />}
+		{!!label && <span>{label}</span>}
+	</label>
+)
 
 Checkbox.propTypes = {
 	label: string,
