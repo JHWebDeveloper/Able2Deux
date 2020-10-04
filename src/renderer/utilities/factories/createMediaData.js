d18776146105dfb34963f4d5f41b9febb1a3c068
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-04 14:58:23 -0400

    simplified createMediaData by moving defaults outside of function scope and accessing through closure

--- src/renderer/utilities/factories/createMediaData.js
+++ src/renderer/utilities/factories/createMediaData.js
@@ -1,92 +1,88 @@
 import { v1 as uuid } from 'uuid'
 import { PENDING } from '../../status/types'
 
-export const createMediaData = ({
-	id,
-	status,
-	acquisitionType = false,
-	mediaType = 'video',
-	url = false,
-	sourceFilePath = false,
-	title = '',
-	filename = ''
-}) => {
-	id = id || uuid()
-	status = status || PENDING
+const defaultMediaData = {
+	id: '',
+	refId: '',
+	status: PENDING,
+	acquisitionType: 'video',
+	mediaType: '',
+	url: '',
+	sourceFilePath: '',
+	tempFilePath: '',
+	download: {
+		eta: '00:00:00',
+		percent: '0%'
+	},
+	title: '',
+	filename: '',
+	isLive: false,
+	exportFilename: '',
+	thumbnail: '',
+	duration: 0,
+	width: 0,
+	height: 0,
+	aspectRatio: '',
+	hasAlpha: false,
+	fps: 0,
+	timecode: 0,
+	start: {
+		enabled: false,
+		display: '00:00:00',
+		tc: 0
+	},
+	end: {
+		enabled: false,
+		display: '00:00:00',
+		tc: 0
+	},
+	arc: 'none',
+	background: 'blue',
+	overlay: 'none',
+	source: {
+		sourceName: '',
+		prefix: true,
+		onTop: false,
+		data: ''
+	},
+	centering: 0,
+	position: {
+		x: 0,
+		y: 0
+	},
+	scale: {
+		x: 100,
+		y: 100,
+		link: true
+	},
+	crop: {
+		t: 0,
+		r: 0,
+		b: 0,
+		l: 0,
+		linkTB: true,
+		linkLR: true
+	},
+	rotation: {
+		angle: '',
+		reflect: ''
+	},
+	audio: {
+		exportAs: 'video_audio',
+		format: 'wav'
+	},
+	render: {
+		status: PENDING,
+		percent: 0
+	}
+}
+
+export const createMediaData = params => {
+	params.id = params.id || uuid()
+	params.refId = params.id
 
 	return {
-		id,
-		refId: id,
-		status,
-		acquisitionType,
-		mediaType,
-		url,
-		sourceFilePath,
-		tempFilePath: false,
-		download: {
-			eta: '00:00:00',
-			percent: '0%'
-		},
-		title,
-		filename,
-		isLive: false,
-		exportFilename: false,
-		thumbnail: false,
-		duration: 0,
-		width: 0,
-		height: 0,
-		aspectRatio: false,
-		hasAlpha: false,
-		fps: 0,
-		timecode: 0,
-		start: {
-			enabled: false,
-			display: '00:00:00',
-			tc: 0
-		},
-		end: {
-			enabled: false,
-			display: '00:00:00',
-			tc: 0
-		},
-		arc: 'none',
-		background: 'blue',
-		overlay: 'none',
-		source: {
-			sourceName: '',
-			prefix: true,
-			onTop: false,
-			data: ''
-		},
-		centering: 0,
-		position: {
-			x: 0,
-			y: 0
-		},
-		scale: {
-			x: 100,
-			y: 100,
-			link: true
-		},
-		crop: {
-			t: 0,
-			r: 0,
-			b: 0,
-			l: 0,
-			linkTB: true,
-			linkLR: true
-		},
-		rotation: {
-			angle: '',
-			reflect: ''
-		},
-		audio: {
-			exportAs: 'video_audio',
-			format: 'wav'
-		},
-		render: {
-			status: PENDING,
-			percent: 0
-		}
+		...defaultMediaData,
+		...params
 	}
 }
