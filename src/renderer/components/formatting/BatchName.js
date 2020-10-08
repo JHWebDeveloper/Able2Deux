e5a0b2cef64ce2fbebe7a27ec616fc30f49e916f
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-08 17:17:15 -0400

    moved two inline functions to callbacks updateBatchName and updateBatchNamePosition

--- src/renderer/components/formatting/BatchName.js
+++ src/renderer/components/formatting/BatchName.js
@@ -1,43 +1,53 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { exact, func, string, oneOf } from 'prop-types'
 
 import { updateNestedStateFromEvent } from '../../actions'
 
 import RadioSet from '../form_elements/RadioSet'
 
-const BatchName = ({ batch, dispatch }) => (
-	<div id="batch-name">
-		<h2>Batch Name</h2>
-		<div>
-			<input
-				type="text"
-				name="name"
-				className="underline"
-				value={batch.name}
-				maxLength={251}
-				onChange={e => dispatch(updateNestedStateFromEvent('batch', e))}
-				placeholder="If none, leave blank" />
-			<RadioSet
-				name="position"
-				state={batch.position}
-				onChange={e => dispatch(updateNestedStateFromEvent('batch', e))}
-				buttons={[
-					{
-						label: 'Replace',
-						value: 'replace'
-					},
-					{
-						label: 'Prepend',
-						value: 'prepend'
-					},
-					{
-						label: 'Append',
-						value: 'append'
-					}
-				]}/>
+const BatchName = ({ batch, dispatch }) => {
+	const updateBatchName = useCallback(e => {
+		dispatch(updateNestedStateFromEvent('batch', e))
+	}, [])
+
+	const updateBatchNamePosition = useCallback(e => {
+		dispatch(updateNestedStateFromEvent('batch', e))
+	}, [])
+
+	return (
+		<div id="batch-name">
+			<h2>Batch Name</h2>
+			<div>
+				<input
+					type="text"
+					name="name"
+					className="underline"
+					value={batch.name}
+					maxLength={251}
+					onChange={updateBatchName}
+					placeholder="If none, leave blank" />
+				<RadioSet
+					name="position"
+					state={batch.position}
+					onChange={updateBatchNamePosition}
+					buttons={[
+						{
+							label: 'Replace',
+							value: 'replace'
+						},
+						{
+							label: 'Prepend',
+							value: 'prepend'
+						},
+						{
+							label: 'Append',
+							value: 'append'
+						}
+					]}/>
+			</div>
 		</div>
-	</div>
-)
+	)
+}
 
 BatchName.propTypes = {
 	batch: exact({
