d2c9e6e4d9dc2d86dc2ace463c3c9549caa122f1
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-15 17:20:16 -0400

    removed css rule, forgot to save before last commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,34 +1,11 @@
 const { merge } = require('webpack-merge')
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const postcssPresetEnv = require('postcss-preset-env')
 const { spawn } = require('child_process')
 
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
 	mode: 'development',
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								plugins: [
-									postcssPresetEnv({ stage: 0 })
-								]
-							}
-						}
-					}
-				]
-			}
-		]
-	},
 	devServer: {
 		port: 3000,
 		hot: true,
