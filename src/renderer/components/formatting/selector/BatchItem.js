91ae78a41e8c9e2e292798ebc9e3a60c2615ac83
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-20 02:25:14 -0400

    replaced selectNeighbor with dispatch to action selectMedia. replaced props isFirst isLast and isOnly with booleans taken from props nextId and prevId

--- src/renderer/components/formatting/selector/BatchItem.js
+++ src/renderer/components/formatting/selector/BatchItem.js
@@ -15,9 +15,10 @@ const { interop } = window.ABLE2
 const ctrlOrCmdKey = interop.isMac ? 'metaKey' : 'ctrlKey'
 
 const BatchItem = props => {
-	const { id, refId, title, selected, index, isFirst, isLast, isOnly, dispatch } = props
-	const triggers = [title, index, isFirst, isLast, isOnly]
-	
+	const { id, refId, title, selected, index, prevId, nextId, dispatch } = props
+	const triggers = [title, index, prevId, nextId]
+	const isOnly = !prevId && !nextId
+
 	const dropdown = useMemo(() => [
 		{
 			label: 'Copy All Settings',
@@ -43,14 +44,14 @@ const BatchItem = props => {
 		{ role: 'spacer' },
 		{
 			label: 'Move Up',
-			hide: isFirst,
+			hide: !prevId,
 			action() {
 				dispatch(moveMedia(index, index - 1))
 			}
 		},
 		{
 			label: 'Move Down',
-			hide: isLast,
+			hide: !nextId,
 			action() {
 				dispatch(moveMedia(index, index + 2))
 			}
@@ -84,18 +85,18 @@ const BatchItem = props => {
 			dropdown[0].action() // Copy All Settings
 		} else if (ctrl && !isOnly && e.key === 'v') {
 			dropdown[1].action() // Paste Settings
-		} else if (ctrl && !isFirst && e.key === 'ArrowUp') {
+		} else if (ctrl && prevId && e.key === 'ArrowUp') {
 			dropdown[4].action() // Move Up
-		} else if (ctrl && !isLast && e.key === 'ArrowDown') {
+		} else if (ctrl && nextId && e.key === 'ArrowDown') {
 			dropdown[5].action() // Move Down
 		} else if (ctrl && e.key === 'd') {
 			dropdown[7].action() // Duplicate Media
 		} else if (e.key === 'Backspace' || e.key === 'Delete') {
 			dropdown[8].action() // Remove Media
-		} else if (!isFirst && e.key === 'ArrowUp') {
-			props.selectNeighbor(index - 1)
-		} else if (!isLast && e.key === 'ArrowDown') {
-			props.selectNeighbor(index + 1)
+		} else if (prevId && e.key === 'ArrowUp') {
+			dispatch(selectMedia(prevId))
+		} else if (nextId && e.key === 'ArrowDown') {
+			dispatch(selectMedia(nextId))
 		}
 	}, triggers)
 
@@ -133,13 +134,11 @@ BatchItem.propTypes = {
 	tempFilePath: string.isRequired,
 	index: number.isRequired,
 	selected: bool.isRequired,
-	isFirst: bool.isRequired,
-	isLast: bool.isRequired,
-	isOnly: bool.isRequired,
+	prevId: string.isRequired,
+	nextId: string.isRequired,
 	copyAllSettings: func.isRequired,
 	applyToAllWithWarning: func.isRequired,
 	removeMediaWithWarning: func.isRequired,
-	selectNeighbor: func.isRequired,
 	dispatch: func.isRequired
 }
 
