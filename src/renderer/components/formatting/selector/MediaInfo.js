ee2f2b89094e43d35af4aa15467a7033d2013b0c
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-21 19:32:56 -0400

    replaced scrollText callbacks with scrollText utility function

--- src/renderer/components/formatting/selector/MediaInfo.js
+++ src/renderer/components/formatting/selector/MediaInfo.js
@@ -1,15 +1,13 @@
-import React, { memo, useCallback, useEffect, useRef } from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 import { number, string } from 'prop-types'
 
 import {
 	compareProps,
 	secondsToTC,
+	scrollText,
 	zeroizeAuto,
 	capitalize,
-	createAnimator
 } from 'utilities'
-
-const textReveal = createAnimator()
 
 const MediaInfo = memo(props => {
 	const {
@@ -27,34 +25,14 @@ const MediaInfo = memo(props => {
 
 	const ref = useRef()
 
-	const panTitle = useCallback(pause => {
-		if (ref.current.scrollWidth === ref.current.clientWidth) {
-			ref.current.style.textOverflow = 'clip'
-			pause()
-		} else {
-			ref.current.innerText = ref.current.innerText.slice(1)
-		}
-	}, [])
-
-	const resetPanTitle = useCallback(() => {
-		ref.current.innerText = ref.current.dataset.title
-		ref.current.style.removeProperty('text-overflow')
-	}, [])
-
 	useEffect(() => {
-		textReveal
-			.onFrame(panTitle, 10)
-			.onStop(resetPanTitle)
+		scrollText(ref.current)
 	}, [])
 
 	return (
 		<div>
 			<img src={thumbnail} alt={title} />
-			<h2
-				data-title={title}
-				ref={ref}
-				onMouseEnter={textReveal.start}
-				onMouseLeave={textReveal.stop}>{title}</h2>
+			<h2 ref={ref}>{title}</h2>
 			<ul>
 				{!!duration && <li>{secondsToTC(duration)};{zeroizeAuto(Math.round(duration % 1 * fps), fps)}</li>}
 				{!!width && !!height && <li>{width}x{height}</li>}
