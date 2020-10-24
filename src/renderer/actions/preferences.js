e29238fb8b95c85d39847ae682703c672a6c870b
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-23 23:19:53 -0400

    moved prefs saving function to reducer and changed actions fixLocationsAndSave and disableWarningAndSave accordingly

--- src/renderer/actions/preferences.js
+++ src/renderer/actions/preferences.js
@@ -17,60 +17,21 @@ export const loadPrefs = () => async dispatch => {
 	}
 }
 
-const savePrefs = async prefs => {
-	try {
-		await interop.savePrefs(prefs)
-	} catch (err) {
-		toastr.error('Preferences failed to save', false, toastrOpts)
-		return false
-	}
-
-	return true
-}
-
-export const fixLocationsAndSavePrefs = (prefs, saveAndClose) => async dispatch => {
-	const newPrefs = {
-		...prefs,
-		saveLocations: prefs.saveLocations
-			.filter(loc => loc.directory)
-			.map(loc => loc.label ? loc : {
-				...loc,
-				label: loc.directory.split('/').pop()
-			})
-	}
-
-	dispatch({
-		type: ACTION.UPDATE_STATE,
-		payload: newPrefs
-	})
-
-	const saved = await savePrefs(newPrefs)
-
-	if (!saved) return false
-
-	if (saveAndClose) {
-		interop.closeCurrentWindow()
-	} else {
-		toastr.success('Preferences saved', false, { ...toastrOpts, timeOut: 2000 })
-	}
-}
-
-export const disableWarningAndSavePrefs = (prefs, warning) => dispatch => {
-	const newPrefs = {
-		...prefs,
-		warnings: {
-			...prefs.warnings,
-			[warning]: false
+export const fixLocationsAndSave = saveAndClose => ({
+	type: ACTION.FIX_LOCATIONS_AND_SAVE,
+	callback() {
+		if (saveAndClose) {
+			interop.closeCurrentWindow()
+		} else {
+			toastr.success('Preferences saved', false, { ...toastrOpts, timeOut: 2000 })
 		}
 	}
+})
 
-	dispatch({
-		type: ACTION.UPDATE_STATE,
-		payload: newPrefs
-	})
-
-	savePrefs(newPrefs)
-}
+export const disableWarningAndSave = warning => ({
+	type: ACTION.DISABLE_WARNING_AND_SAVE,
+	payload: { warning }
+})
 
 export const updateLocationField = (id, name, value) => ({
 	type: ACTION.UPDATE_LOCATION_FIELD,
