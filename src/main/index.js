6b9c332a9f7bb54ac5897228eaddaa6356375e74
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-24 15:34:42 -0400

    in function openWindow set enableRemoteModule to false. finally got rid of all remote module instances

--- src/main/index.js
+++ src/main/index.js
@@ -40,7 +40,7 @@ const openWindow = opts => new BrowserWindow({
 		nodeIntegration: dev,
 		contextIsolation: !dev,
 		enableEval: false,
-		enableRemoteModule: true,
+		enableRemoteModule: false,
 		preload: dev
 			? path.join(__dirname, 'preload', 'babelRegister.js')
 			: path.join(__dirname, 'preload.js')
