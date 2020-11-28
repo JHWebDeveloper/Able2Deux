50927d88f1388182037352e326c115f9d5046b0c
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-11-28 13:26:26 -0500

    removed 4ths and 3rds on snap points, changed my mind

--- src/renderer/components/formatting/editor/Crop.js
+++ src/renderer/components/formatting/editor/Crop.js
@@ -21,7 +21,7 @@ const propsLStatic = { name: 'l', title: 'Crop Left' }
 const propsRStatic = { name: 'r', title: 'Crop Right' }
 
 const sliderProps = {
-	snapPoints: [25, 33.33, 50, 66.67, 75],
+	snapPoints: [50],
 	sensitivity: 2,
 	enableAutoCenter: true
 }
