14e00bb3d912ff6d021f7e3d0c844d19308ccad1
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-11-29 17:03:40 -0500

    first commit

---
+++ src/renderer/components/acquisition/ScreenRecorderTimer.js
@@ -0,0 +1,36 @@
+import React, { useCallback, useState } from 'react'
+
+import Checkbox from '../form_elements/Checkbox'
+import TimecodeInput from '../form_elements/TimecodeInput'
+import Clock from '../form_elements/Clock'
+
+const ScreenRecorderTimer = ({ timer, setTimer, screenshot, recording }) => {
+	const [ enabled, toggleTimer ] = useState(false)
+
+	const updateTimecode = useCallback(({ value }) => setTimer(value), [])
+
+	return (
+		<div className="timecode">
+			<Checkbox
+				name="enabled"
+				checked={enabled}
+				onChange={() => toggleTimer(!enabled)}
+				disabled={screenshot || recording}
+				switchIcon />
+			{recording ? ( // eslint-disable-line no-extra-parens
+				<Clock
+					start={timer}
+					decrement={enabled} />
+			) : (
+				<TimecodeInput
+					title="Record Timer"
+					value={timer}
+					min={1}
+					onChange={updateTimecode}
+					disabled={!enabled || screenshot || recording} />
+			)}
+		</div>
+	)
+}
+
+export default ScreenRecorderTimer
