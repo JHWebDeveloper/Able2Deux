d1b49f19c282b203804698066ad24926a00d6fb0
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-11-30 02:15:05 -0500

    consolidated components TimecodeInput and TimecodeInputFrames into TimecodeInputTemplate. components were nearly indentical.

--- src/renderer/components/form_elements/TimecodeInput.js
+++ src/renderer/components/form_elements/TimecodeInput.js
@@ -1,81 +1,19 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import { bool, func, number, string } from 'prop-types'
 
-import { clamp, secondsToTC, tcToSeconds, limitTCChars } from 'utilities'
+import { tcToSeconds, secondsToTC, limitTCChars } from 'utilities'
+
+import TimecodeInputTemplate from './TimecodeInputTemplate'
 
 const limitChars = limitTCChars(2)
 
-const TimecodeInput = ({
-	name,
-	title,
-	value = 0,
-	min = 0,
-	max = 86399, // 86399 == 23:59:59
-	disabled = false,
-	onChange
-}) => {
-	const [ display, updateDisplay ] = useState(secondsToTC(value))
-
-	const updateTimecode = useCallback(value => onChange({
-		name,
-		value: clamp(value, min, max)
-	}), [min, max])
-
-	const syncTimecode = useCallback(value => {
-		value = tcToSeconds(value)
-		value = clamp(value, min, max)
-
-		updateDisplay(secondsToTC(value))
-		updateTimecode(value)
-	}, [min, max])
-
-	const pasteTimecode = useCallback(async e => {
-		e.preventDefault()
-	
-		let txt = await navigator.clipboard.readText()
-		
-		txt = txt.replace(/[^:;0-9]/g, '')
-		
-		updateTimecode(tcToSeconds(txt))
-	}, [min, max])
-
-	const onKeyDown = useCallback(e => {
-		switch (e.key) {
-			case 'Enter':
-				e.preventDefault()
-				syncTimecode(e.target.value)
-				break
-			case 'ArrowUp':
-				e.preventDefault()
-				updateTimecode(value + 1)
-				break
-			case 'ArrowDown':
-				e.preventDefault()
-				updateTimecode(value - 1)
-				break
-			default:
-				return true
-		}
-	}, [value, min, max])
-
-	useEffect(() => {
-		updateDisplay(secondsToTC(value))
-	}, [value])
-
-	return (
-		<input
-			type="text"
-			className="monospace"
-			title={title}
-			value={display}
-			onKeyPress={limitChars}
-			onKeyDown={onKeyDown}
-			onChange={e => updateDisplay(e.target.value)}
-			onBlur={e => syncTimecode(e.target.value)}
-			onPaste={pasteTimecode}
-			disabled={disabled} />
-	)
-}
+const TimecodeInput = props => (
+	<TimecodeInputTemplate
+		tcStringToNumber={tcToSeconds}
+		numberToTCString={secondsToTC}
+		limitChars={limitChars}
+		{...props} />
+)
 
 TimecodeInput.propTypes = {
 	name: string,
@@ -83,7 +21,7 @@ TimecodeInput.propTypes = {
 	timecode: string,
 	value: number,
 	min: number,
-	max: number,
+	max: number.isRequired,
 	disabled: bool,
 	onChange: func.isRequired
 }
--- src/renderer/components/form_elements/TimecodeInputFrames.js
+++ src/renderer/components/form_elements/TimecodeInputFrames.js
@@ -1,87 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import { func, number, string } from 'prop-types'
 
-import { clamp, framesToTC, tcToFrames, limitTCChars } from 'utilities'
+import { framesToTC, tcToFrames, limitTCChars } from 'utilities'
+
+import TimecodeInputTemplate from './TimecodeInputTemplate'
 
 const limitChars = limitTCChars(3)
 
-const TimecodeInputFrames = ({
-	id,
-	name,
-	value = 0,
-	fps = 59.94,
-	min = 0,
-	max,
-	onChange
-}) => {
-	const [ display, updateDisplay ] = useState(framesToTC(value, fps))
-
-	const updateTimecode = useCallback(value => onChange({
-		name,
-		value: clamp(value, min, max)
-	}), [min, max])
-
-	const syncTimecode = useCallback(value => {
-		value = tcToFrames(value, fps)
-		value = clamp(value, min, max)
-
-		updateDisplay(framesToTC(value, fps))
-		updateTimecode(value)
-	}, [min, max])
-
-	const pasteTimecode = useCallback(async e => {
-		e.preventDefault()
-	
-		let txt = await navigator.clipboard.readText()
-		
-		txt = txt.replace(/[^:;0-9]/g, '')
-		
-		updateTimecode(tcToFrames(txt, fps))
-	}, [min, max])
-
-	const onKeyDown = useCallback(e => {
-		switch (e.key) {
-			case 'Enter':
-				e.preventDefault()
-				syncTimecode(e.target.value)
-				break
-			case 'ArrowUp':
-				e.preventDefault()
-				updateTimecode(value + 1)
-				break
-			case 'ArrowDown':
-				e.preventDefault()
-				updateTimecode(value - 1)
-				break
-			default:
-				return true
-		}
-	}, [value, min, max])
-
-	useEffect(() => {
-		updateDisplay(framesToTC(value, fps))
-	}, [value])
-
-	return (
-		<input
-			type="text"
-			className="monospace"
-			title="HH:MM:SS:FF"
-			id={id}
-			name={name}
-			value={display}
-			onKeyPress={limitChars}
-			onKeyDown={onKeyDown}
-			onChange={e => updateDisplay(e.target.value)}
-			onBlur={e => syncTimecode(e.target.value)}
-			onPaste={pasteTimecode} />
-	)
-}
+const TimecodeInputFrames = props => (
+	<TimecodeInputTemplate
+		tcStringToNumber={tcToFrames}
+		numberToTCString={framesToTC}
+		limitChars={limitChars}
+		{...props} />
+)
 
 TimecodeInputFrames.propTypes = {
 	id: string,
 	name: string,
-	timecode: string,
 	value: number,
 	fps: number,
 	min: number,
