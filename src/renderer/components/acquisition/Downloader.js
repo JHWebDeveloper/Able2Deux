558b0abde245889af50540ae88793cf0d8ebc9ad
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-12-04 12:24:06 -0500

    moved inline functions to useCallback, moved inline url regex to useMemo

--- src/renderer/components/acquisition/Downloader.js
+++ src/renderer/components/acquisition/Downloader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { bool, func, number, oneOfType, string } from 'prop-types'
 
 import { updateStateFromEvent, download } from 'actions'
@@ -7,42 +7,54 @@ import RadioSet from '../form_elements/RadioSet'
 
 const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
 
-const Downloader = ({ url, optimize, output, disableRateLimit, dispatch }) => (
-	<div id="downloader">
-		<input
-			type="text"
-			name="url"
-			className="underline"
-			value={url}
-			onChange={e => dispatch(updateStateFromEvent(e))}
-			placeholder="Paste URL here..." />
-		<button
-			type="button"
-			className="app-button"
-			name="download"
-			title="Download Video"
-			disabled={!urlRegex.test(url)}
-			onClick={() => dispatch(download({ url, optimize, output, disableRateLimit }))}>
-			Download
-		</button>
-		<span>
-			<RadioSet
-				name="optimize"
-				state={optimize}
-				onChange={e => dispatch(updateStateFromEvent(e))}
-				buttons={[
-					{
-						label: 'Optimize Video Quality',
-						value: 'quality'
-					},
-					{
-						label: 'Optimize Download Time',
-						value: 'download'
-					}
-				]} />
-		</span>
-	</div>
-)
+const Downloader = ({ url, optimize, output, disableRateLimit, dispatch }) => {
+	const badURL = useMemo(() => !urlRegex.test(url), [url])
+
+	const downloadWithSettings = useCallback(() => {
+		dispatch(download({ url, optimize, output, disableRateLimit }))
+	}, [url, optimize, output, disableRateLimit])
+
+	const dispatchWithEvent = useCallback(e => {
+		dispatch(updateStateFromEvent(e))
+	}, [])
+
+	return (
+		<div id="downloader">
+			<input
+				type="text"
+				name="url"
+				className="underline"
+				value={url}
+				onChange={dispatchWithEvent }
+				placeholder="Paste URL here..." />
+			<button
+				type="button"
+				className="app-button"
+				name="download"
+				title="Download Video"
+				disabled={badURL }
+				onClick={downloadWithSettings}>
+				Download
+			</button>
+			<span>
+				<RadioSet
+					name="optimize"
+					state={optimize}
+					onChange={dispatchWithEvent}
+					buttons={[
+						{
+							label: 'Optimize Video Quality',
+							value: 'quality'
+						},
+						{
+							label: 'Optimize Download Time',
+							value: 'download'
+						}
+					]} />
+			</span>
+		</div>
+	)
+}
 
 Downloader.propTypes = {
 	url: string.isRequired,
