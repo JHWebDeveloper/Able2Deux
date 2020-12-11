a4322dcf40e4c0c518ba5d470af78226506911cc
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-12-11 00:58:38 -0500

    wrapped component in PrefsPanel component. moved warnings to separate component. added settings for slider snap to point and split duration.

--- src/renderer/components/preferences/Defaults.js
+++ src/renderer/components/preferences/Defaults.js
@@ -3,98 +3,85 @@ import { bool, exact, func, number, string } from 'prop-types'
 
 import { updateState, updateStateFromEvent, toggleCheckbox, toggleNestedCheckbox } from 'actions'
 
-import Checkbox from '../form_elements/Checkbox'
+import PrefsPanel from './PrefsPanel'
 import NumberInput from '../form_elements/NumberInput'
+import Checkbox from '../form_elements/Checkbox'
+import TimecodeInputSeconds from '../form_elements/TimecodeInputSeconds'
 
-const Defaults = ({ warnings, editAll, enableWidescreenGrids, gridColor, scaleSliderMax, dispatch }) => {
-	const updateScaleMax = useCallback(({ name, value }) => {
+const FormattingSettings = props => {
+	const { dispatch } = props
+
+	const toggleCheckboxDispatch = useCallback(e => {
+		dispatch(toggleCheckbox(e))
+	}, [])
+
+	const updateStateDispatch = useCallback(({ name, value }) => {
 		dispatch(updateState({ [name]: value }))
 	}, [])
 
-	const toggleWarning = useCallback(e => {
-		dispatch(toggleNestedCheckbox('warnings', e))
-	}, [])
-
 	return (
-		<div id="defaults">
-			<fieldset>
-				<legend>Warnings and Defaults</legend>
-				<div>
-					<Checkbox
-						label="Remove Warning"
-						name="remove"
-						checked={warnings.remove}
-						onChange={toggleWarning}
-						switchIcon />
-					<Checkbox
-						label="Remove All Warning"
-						name="removeAll"
-						checked={warnings.removeAll}
-						onChange={toggleWarning}
-						switchIcon />
-					<Checkbox
-						label="Apply to All Warning"
-						name="applyToAll"
-						checked={warnings.applyToAll}
-						onChange={toggleWarning}
-						switchIcon />
-					<Checkbox
-						label="Source on Top Warning"
-						name="sourceOnTop"
-						checked={warnings.sourceOnTop}
-						onChange={toggleWarning}
-						switchIcon />
-					<Checkbox
-						label="Edit All by Default"
-						name="editAll"
-						checked={editAll}
-						onChange={e => dispatch(toggleCheckbox(e))}
-						switchIcon />
-					<Checkbox
-						label="Widescreen Grids"
-						name="enableWidescreenGrids"
-						checked={enableWidescreenGrids}
-						onChange={e => dispatch(toggleCheckbox(e))}
-						switchIcon />
-					<span className="input-option">
-						<label htmlFor="grid-color">Grid Color</label>
-						<input
-							type="color"
-							name="gridColor"
-							id="grid-color"
-							value={gridColor}
-							onChange={e => dispatch(updateStateFromEvent(e))} />
-					</span>
-					<span className="input-option">
-						<label htmlFor="scaleSliderMax">Scale Max</label>
-						<NumberInput
-							name="scaleSliderMax"
-							id="scaleSliderMax"
-							value={scaleSliderMax}
-							min={100}
-							max={4500}
-							fineTuneStep={1}
-							defaultValue={400}
-							onChange={updateScaleMax} />
-					</span>
-				</div>
-			</fieldset>
-		</div>
+		<PrefsPanel title="Formatting Settings" className="span-third">
+			<Checkbox
+				label="Edit All by Default"
+				name="editAll"
+				checked={props.editAll}
+				onChange={toggleCheckboxDispatch}
+				switchIcon />
+			<Checkbox
+				label="Slider Snap Points"
+				name="snapToPoint"
+				checked={props.snapToPoint}
+				onChange={toggleCheckboxDispatch}
+				switchIcon />
+			<Checkbox
+				label="Widescreen Grids"
+				name="enableWidescreenGrids"
+				checked={props.enableWidescreenGrids}
+				onChange={toggleCheckboxDispatch}
+				switchIcon />
+			<span className="input-option">
+				<label htmlFor="grid-color">Grid Color</label>
+				<input
+					type="color"
+					name="gridColor"
+					id="grid-color"
+					value={props.gridColor}
+					onChange={e => dispatch(updateStateFromEvent(e))} />
+			</span>
+			<span>
+				<label htmlFor="split">Split Duration</label>
+				<TimecodeInputSeconds
+					name="split"
+					id="split"
+					value={props.split}
+					min={1}
+					max={86399}
+					onChange={updateStateDispatch} />
+			</span>
+			<span>
+				<label htmlFor="scaleSliderMax">Scale Slider Max</label>
+				<NumberInput
+					name="scaleSliderMax"
+					id="scaleSliderMax"
+					value={props.scaleSliderMax}
+					min={100}
+					max={4500}
+					fineTuneStep={1}
+					defaultValue={400}
+					onChange={updateStateDispatch} />
+			</span>
+		</PrefsPanel>
 	)
 }
 
-Defaults.propTypes = {
-	warnings: exact({
-		remove: bool,
-		removeAll: bool,
-		applyToAll: bool,
-		sourceOnTop: bool
-	}).isRequired,
+FormattingSettings.propTypes = {
 	editAll: bool.isRequired,
+	snapToPoint: bool.isRequired,
 	enableWidescreenGrids: bool.isRequired,
 	gridColor: string.isRequired,
+	split: number.isRequired,
 	scaleSliderMax: number.isRequired,
 	dispatch: func.isRequired
 }
 
-export default Defaults
+export default FormattingSettings
