0fd5067585967dc892b3df66ab9397285d1993e6
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-12-26 14:29:01 -0500

    moved grid button to new component GridSelector

--- src/renderer/components/formatting/preview/Controls.js
+++ src/renderer/components/formatting/preview/Controls.js
@@ -1,111 +1,68 @@
 import React, { useCallback } from 'react'
-import { bool, func, number, exact, shape, string } from 'prop-types'
+import { bool, func, number, object, shape, string } from 'prop-types'
+
+import { updateMediaState } from 'actions'
 
 import FrameSelector from './FrameSelector'
-
-const toggleTitle = state => state ? 'Hide' : 'Show'
+import GridSelector from './GridSelector'
 
 const Controls = props => {
-	const { selected, grids, gridColor, dispatch } = props
+	const { selected, dispatch } = props
+	const { id, timecode, start, end, totalFrames } = selected
 
-	const toggleColor = useCallback(gridName => ({
-		color: gridName ? gridColor : '#eee'
-	}), [gridColor])
+	const onKeyPress = useCallback(e => {
+		const props = {}
+		
+		switch (e.key) {
+			case 'i':
+			case 'e':
+				if (timecode < end) props.start = timecode
+				break
+			case 'o':
+			case 'r':
+				if (timecode >= start) props.end = timecode + 1
+				break
+			case 'd':
+				props.start = 0
+				break
+			case 'g':
+				props.start = 0
+				// falls through
+			case 'f':
+				props.end = totalFrames
+				break
+			case 'q':
+				props.timecode = start
+				break
+			case 'w':
+				props.timecode = end - 1
+				break
+			default:
+				return true
+		}
 
-	const toggleGrid = useCallback(e => {
-		props.toggleGrids({
-			...grids,
-			[e.target.name]: !grids[e.target.name]
-		})
-	}, [grids])
+		dispatch(updateMediaState(id, props))
+	}, [id, timecode, start, end, totalFrames])
 
 	return (
-		<>
+		<div id="preview-controls" onKeyPress={onKeyPress}>
 			{selected.mediaType === 'video' && (
 				<FrameSelector
 					selected={selected}
 					dispatch={dispatch} />
 			)}
-			<button
-				type="button"
-				className="symbol"
-				name="grid"
-				title={`${toggleTitle(grids.grid)} Grid`}
-				style={{
-					...toggleColor(grids.grid),
-					gridColumn: props.enableWidescreenGrids ? 6 : 8
-				}}
-				onClick={toggleGrid}>grid_on</button>
-			{props.enableWidescreenGrids && <>
-				<button
-					type="button"
-					title={`${toggleTitle(grids._239)}  2.39:1 Markers`}
-					className="monospace"
-					name="_239"
-					style={toggleColor(grids._239)}
-					onClick={toggleGrid}>
-					<span>2.39</span>
-				</button>
-				<button
-					type="button"
-					title={`${toggleTitle(grids._185)}  1.85:1 Markers`}
-					className="monospace"
-					name="_185"
-					style={toggleColor(grids._185)}
-					onClick={toggleGrid}>
-					<span>1.85</span>
-				</button>
-			</>}
-			<button
-				type="button"
-				title={`${toggleTitle(grids._43)} 4:3 Markers`}
-				className="monospace"
-				name="_43"
-				style={toggleColor(grids._43)}
-				onClick={toggleGrid}>
-				<span>4:3</span>
-			</button>
-			<button
-				type="button"
-				title={`${toggleTitle(grids._11)}  1:1 Markers`}
-				className="monospace"
-				name="_11"
-				style={toggleColor(grids._11)}
-				onClick={toggleGrid}>
-				<span>1:1</span>
-			</button>
-			<button
-				type="button"
-				title={`${toggleTitle(grids._916)}  9:16 Markers`}
-				className="monospace"
-				name="_916"
-				style={toggleColor(grids._916)}
-				onClick={toggleGrid}>
-				<span>9:16</span>
-			</button>
-		</>
+			<GridSelector
+				grids={props.grids}
+				enableWidescreenGrids={props.enableWidescreenGrids}
+				gridColor={props.gridColor}
+				toggleGrids={props.toggleGrids} />
+		</div>
 	)
 }
 
 Controls.propTypes = {
-	selected: shape({
-		id: string,
-		mediaType: string,
-		timecode: number,
-		start: number,
-		end: number,
-		fps: number,
-		duration: number,
-		totalFrames: number
-	}).isRequired,
-	grids: exact({
-		grid: bool,
-		_239: bool,
-		_185: bool,
-		_43: bool,
-		_11: bool,
-		_916: bool
-	}).isRequired,
+	selected: object.isRequired,
+	grids: object.isRequired,
 	gridColor: string.isRequired,
 	enableWidescreenGrids: bool.isRequired,
 	toggleGrids: func,
