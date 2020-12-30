7e29bc82d1875f6ed3d0b12f558273d10de9b6d6
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-12-29 22:47:01 -0500

    first commit

---
+++ src/renderer/components/formatting/preview/PreviewCanvas.js
@@ -0,0 +1,50 @@
+import React, { createRef, useCallback, useEffect } from 'react'
+
+import {} from 'actions'
+import { rgbToHex } from 'utilities'
+
+const img = new Image()
+let cnv = false
+let ctx = false
+
+const PreviewCanvas = ({ previewStill, eyedropper, setEyedropToBgColor }) => {
+	const ref = createRef()
+
+	const getColorAtClickPos = useCallback(e => {
+		const { left, top } = cnv.getBoundingClientRect()
+		const x = e.clientX - left
+		const y = e.clientY - top
+	
+		const rgb = ctx.getImageData(x, y, 1, 1).data
+		const hex = rgbToHex(...rgb)
+
+		setEyedropToBgColor(hex)
+	}, [setEyedropToBgColor])
+
+	useEffect(() => {
+		cnv = ref.current
+		ctx = cnv.getContext('2d')
+
+		cnv.width = 384
+		cnv.height = 216
+
+		img.onload = () => {
+			ctx.drawImage(img, 0, 0, cnv.width, cnv.height)
+		}
+	}, [])
+
+	useEffect(() => {
+		img.src = previewStill
+	}, [previewStill])
+
+	const eyedropperProps = eyedropper ? {
+		className: 'eyedropper',
+		onClick: getColorAtClickPos
+	} : {}
+
+	return (
+		<canvas ref={ref} {...eyedropperProps} ></canvas>
+	)
+}
+
+export default PreviewCanvas
