944e2ff75d19b1bbd909d4b3369f7ae8c99827f0
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2021-07-16 11:40:17 -0400

    readded placeholder import, must have been deleted accidentally

--- src/main/modules/utilities/base64Encode.js
+++ src/main/modules/utilities/base64Encode.js
@@ -1,5 +1,7 @@
 import { promises as fsp } from 'fs'
 
+import { placeholder } from '.'
+
 export const base64Encode = async file => `data:image/png;base64,${await fsp.readFile(file, 'base64')}`
 
 export const base64EncodeOrPlaceholder = async file => {
