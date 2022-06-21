4590dae03c6cbf80d394d64ac95588c8e73c85b7
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-06-20 22:34:01 -0400

    removed importing of ts-ebml and function fixDuration. Replacing ts-ebml with fix-webm-duration in preload script screenRecorder.js

--- src/main/modules/acquisition/screenRecorder.js
+++ src/main/modules/acquisition/screenRecorder.js
@@ -1,7 +1,6 @@
 import { desktopCapturer } from 'electron'
 import { promises as fsp } from 'fs'
 import path from 'path'
-import { Decoder, Reader, tools } from 'ts-ebml'
 
 import { scratchDisk } from '../scratchDisk'
 
@@ -35,33 +34,8 @@ export const getRecordSources = async () => {
 		.flat()
 }
 
-const fixDuration = buffer => {
-	const decoder = new Decoder()
-	const reader = new Reader()
-
-	reader.logging = false
-	reader.drop_default_duration = false
-
-	const elms = decoder.decode(buffer)
-	const len = elms.length
-
-	for (let i = 0; i < len; i++) {
-		reader.read(elms[i])
-	}
-
-	reader.stop()
-
-	const metadata = tools.makeMetadataSeekable(reader.metadatas, reader.duration, reader.cues)
-	const body = buffer.slice(reader.metadataSize)
-	
-	return Buffer.concat([
-		Buffer.from(metadata),
-		Buffer.from(body)
-	])
-}
-
 export const saveScreenRecording = async ({ id, buffer, screenshot }) => {
-	const fixedBuffer = screenshot ? buffer : fixDuration(buffer)
+	const fixedBuffer = buffer
 	const filePath = path.join(scratchDisk.imports.path, `${id}.${screenshot ? 'png' : 'webm'}`)
 	const encoding = screenshot ? 'base64' : 'utf8'
 
