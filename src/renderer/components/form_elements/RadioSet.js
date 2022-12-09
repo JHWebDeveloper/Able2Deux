b30235499ed65170d633676206cf65f937681cc0
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-09 00:51:41 -0500

    added a conditional fallback to the first radio value if a non existent value is provided.

--- src/renderer/components/form_elements/RadioSet.js
+++ src/renderer/components/form_elements/RadioSet.js
@@ -2,8 +2,19 @@ import React, { useMemo } from 'react'
 import { arrayOf, element, func, shape, string } from 'prop-types'
 import { v1 as uuid } from 'uuid'
 
-const RadioSet = ({ name, state, onChange, buttons }) => {
+const RadioSet = ({ name, state, onChange, buttons = [] }) => {
 	const setKey = useMemo(uuid, [])
+
+	if (buttons.length && !buttons.some(btn => btn.value === state)) {
+		state = buttons[0].value
+
+		onChange({
+			target: {
+				name,
+				value: state
+			}
+		})
+	}
 
 	return buttons.map(({ label, value, component }, i) => (
 		<label key={`${setKey}_${i}`}>
