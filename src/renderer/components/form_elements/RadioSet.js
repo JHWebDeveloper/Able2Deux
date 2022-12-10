32784a889d68a721132126991479e571bf1021c2
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-09 21:21:14 -0500

    removed fallback conditional statement, decided it wasnt needed

--- src/renderer/components/form_elements/RadioSet.js
+++ src/renderer/components/form_elements/RadioSet.js
@@ -5,17 +5,6 @@ import { v1 as uuid } from 'uuid'
 const RadioSet = ({ name, state, onChange, buttons = [] }) => {
 	const setKey = useMemo(uuid, [])
 
-	if (buttons.length && !buttons.some(btn => btn.value === state)) {
-		state = buttons[0].value
-
-		onChange({
-			target: {
-				name,
-				value: state
-			}
-		})
-	}
-
 	return buttons.map(({ label, value, component }, i) => (
 		<label key={`${setKey}_${i}`}>
 			<input
