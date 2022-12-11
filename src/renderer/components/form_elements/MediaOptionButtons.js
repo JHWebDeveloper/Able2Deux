7468b353f087b4623ccd9d1029b4b5dc29aa0baa
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-11 15:03:49 -0500

    first commit

---
+++ src/renderer/components/form_elements/MediaOptionButtons.js
@@ -0,0 +1,27 @@
+import React, { useMemo } from 'react'
+import { v1 as uuid } from 'uuid'
+
+const MediaOptionButtons = ({ buttons }) => {
+  const menuId = useMemo(uuid, [])
+  const enabledButtons = useMemo(() => buttons.filter(({ hide }) => !hide), [buttons])
+
+  return enabledButtons.map(({ type, label, action }, i) => type === 'spacer' ? (
+    <span
+      key={`${menuId}_${i}`}
+      className="spacer"
+      aria-hidden="true"
+      data-no-drag></span>
+  ) : (
+    <button
+      key={`${menuId}_${i}`}
+      type="button"
+      autoFocus={i === 0}
+      onClick={() => {
+        action()
+        toggleRevealMenu(false)
+      }}
+      data-no-drag>{label}</button>
+  ))
+}
+
+export default MediaOptionButtons
