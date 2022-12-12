59c864b5358d182c61d698c62fc04db4fefe4e6e
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-12 18:02:43 -0500

    first commit

---
+++ src/renderer/components/preferences/PreviewSettings.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+
+import { PrefsContext } from 'store/preferences'
+
+import { updateStateFromEvent } from 'actions'
+
+import AspectRatioMarkers from './AspectRatioMarkers'
+
+const PreviewSettings = () => {
+  const { preferences, dispatch } = useContext(PrefsContext)
+
+  return (
+    <form>
+			<span className="input-option">
+				<label htmlFor="grid-color">Grid Color</label>
+				<input
+					type="color"
+					name="gridColor"
+					id="grid-color"
+					value={preferences.gridColor}
+					onChange={e => dispatch(updateStateFromEvent(e))} />
+			</span>
+      <AspectRatioMarkers
+				aspectRatioMarkers={preferences.aspectRatioMarkers}
+				dispatch={dispatch} />
+    </form>
+  )
+}
+
+export default PreviewSettings
