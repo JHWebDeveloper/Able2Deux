3cda8d65d776a9b9180efdc800300b967da6b424
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-12 17:43:51 -0500

    made SaveLocations its own route again.

--- src/renderer/components/preferences/Preferences.js
+++ src/renderer/components/preferences/Preferences.js
@@ -7,9 +7,10 @@ import { PrefsProvider, PrefsContext } from 'store/preferences'
 import { updateState } from 'actions'
 import { toastrOpts, createKonamiListener } from 'utilities'
 
-import RenderOutput from './RenderOutput'
+import Rendering from './RenderOutput'
 import AcquisitionSettings from './AcquisitionSettings'
 import FormattingSettings from './FormattingSettings'
+import SaveLocations from './SaveLocations'
 import Warnings from './Warnings'
 import SaveAndClose from './SaveAndClose'
 
@@ -41,55 +42,21 @@ const PreferencesRouter = () => {
 			<nav>
 				<NavLink to="/" title="Acquisition">Acquisition</NavLink>
 				<NavLink to="/formatting" title="Formatting">Formatting</NavLink>
-				<NavLink to="/renderoutput" title="Render Output">Render Output</NavLink>
+				<NavLink to="/rendering" title="Rendering">Rendering</NavLink>
+				<NavLink to="/save-locations" title="Save Locations">Save Locations</NavLink>
 				<NavLink to="/warnings" title="Warnings">Warnings</NavLink>
 			</nav>
 			<Routes>
 				<Route path="/" element={<AcquisitionSettings />} />
 				<Route path="/formatting" element={<FormattingSettings />} />
-				<Route path="/renderoutput" element={<RenderOutput />} />
+				<Route path="/rendering" element={<Rendering />} />
+				<Route path="/save-locations" element={<SaveLocations />} />
 				<Route path="/warnings" element={<Warnings />} />
 			</Routes>
 			<SaveAndClose dispatch={dispatch} />
 		</HashRouter> 
 	)
 }
-
-			// <AcquisitionSettings
-			// 	optimize={preferences.optimize}
-			// 	screenshot={preferences.screenshot}
-			// 	screenRecorderFrameRate={preferences.screenRecorderFrameRate}
-			// 	timerEnabled={preferences.timerEnabled}
-			// 	timer={preferences.timer}
-			// 	dispatch={dispatch} />
-			// <FormattingSettings
-			// 	editAll={preferences.editAll}
-			// 	sliderSnapPoints={preferences.sliderSnapPoints}
-			// 	aspectRatioMarkers={preferences.aspectRatioMarkers}
-			// 	gridColor={preferences.gridColor}
-			// 	split={preferences.split}
-			// 	animateBackground={preferences.animateBackground}
-			// 	enable11pmBackgrounds={preferences.enable11pmBackgrounds}
-			// 	scaleSliderMax={preferences.scaleSliderMax}
-			// 	dispatch={dispatch} />
-			// <Warnings
-			// 	warnings={preferences.warnings}
-			// 	dispatch={dispatch} />
-			// <ScratchDisk 
-			// 	scratchDisk={preferences.scratchDisk}
-			// 	dispatch={dispatch} />
-			// <RenderOutput
-			// 	renderOutput={preferences.renderOutput}
-			// 	renderFrameRate={preferences.renderFrameRate}
-			// 	customFrameRate={preferences.customFrameRate}
-			// 	autoPNG={preferences.autoPNG}
-			// 	asperaSafe={preferences.asperaSafe}
-			// 	concurrent={preferences.concurrent}
-			// 	dispatch={dispatch} />
-			// <SaveLocations
-			// 	saveLocations={preferences.saveLocations}
-			// 	dispatch={dispatch} />
-			// <SaveAndClose dispatch={dispatch} /> 
 
 const Preferences = () => (
 	<PrefsProvider>
