781f5fbbdfcfe6994f9e456a2f99c20f5220d9e9
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-13 13:19:59 -0500

    added component ScratchDisk and grid color picker

--- src/renderer/components/preferences/GeneralSettings.js
+++ src/renderer/components/preferences/GeneralSettings.js
@@ -2,9 +2,22 @@ import React, { useCallback, useContext } from 'react'
 
 import { PrefsContext } from 'store/preferences'
 
-import { toggleNestedCheckbox } from 'actions'
+import { updateStateFromEvent, toggleNestedCheckbox } from 'actions'
 
+import RadioSet from '../form_elements/RadioSet'
+import ScratchDisk from './ScratchDisk'
 import Checkbox from '../form_elements/Checkbox'
+
+const themeButtons = [
+	{
+		label: 'Classic',
+		value: 'classic'
+	},
+	{
+		label: 'Dark',
+		value: 'dark'
+	}
+]
 
 const Warnings = () => {
 	const { preferences, dispatch } = useContext(PrefsContext)
@@ -16,46 +29,60 @@ const Warnings = () => {
 
 	return (
 		<form>
-			<span className="input-option">
-				<Checkbox
-					label="Remove"
-					name="remove"
-					checked={warnings.remove}
-					onChange={toggleWarning}
-					switchIcon />
-			</span>
-			<span className="input-option">
-				<Checkbox
-					label="Remove All"
-					name="removeAll"
-					checked={warnings.removeAll}
-					onChange={toggleWarning}
-					switchIcon />
-			</span>
-			<span className="input-option">
-				<Checkbox
-					label="Apply to All"
-					name="applyToAll"
-					checked={warnings.applyToAll}
-					onChange={toggleWarning}
-					switchIcon />
-			</span>
-			<span className="input-option">
-				<Checkbox
-					label="Source on Top"
-					name="sourceOnTop"
-					checked={warnings.sourceOnTop}
-					onChange={toggleWarning}
-					switchIcon />
-			</span>
-			<span className="input-option">
-				<Checkbox
-					label="Start Over"
-					name="startOver"
-					checked={warnings.startOver}
-					onChange={toggleWarning}
-					switchIcon />
-			</span>
+			<fieldset>
+				<legend>Theme:</legend>
+				<RadioSet
+					name="theme"
+					state={preferences.theme}
+					onChange={e => dispatch(updateStateFromEvent(e))}
+					buttons={themeButtons} />
+			</fieldset>
+			<ScratchDisk
+				scratchDisk={preferences.scratchDisk}
+				dispatch={dispatch} />
+			<fieldset>
+				<legend>Warnings:</legend>
+				<span className="input-option">
+					<Checkbox
+						label="Remove"
+						name="remove"
+						checked={warnings.remove}
+						onChange={toggleWarning}
+						switchIcon />
+				</span>
+				<span className="input-option">
+					<Checkbox
+						label="Remove All"
+						name="removeAll"
+						checked={warnings.removeAll}
+						onChange={toggleWarning}
+						switchIcon />
+				</span>
+				<span className="input-option">
+					<Checkbox
+						label="Apply to All"
+						name="applyToAll"
+						checked={warnings.applyToAll}
+						onChange={toggleWarning}
+						switchIcon />
+				</span>
+				<span className="input-option">
+					<Checkbox
+						label="Source on Top"
+						name="sourceOnTop"
+						checked={warnings.sourceOnTop}
+						onChange={toggleWarning}
+						switchIcon />
+				</span>
+				<span className="input-option">
+					<Checkbox
+						label="Start Over"
+						name="startOver"
+						checked={warnings.startOver}
+						onChange={toggleWarning}
+						switchIcon />
+				</span>
+			</fieldset>
 		</form>
 	)
 }
