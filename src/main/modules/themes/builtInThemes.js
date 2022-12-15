2676f6642050a1f2728473f8aa378214c7c8ffc7
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-15 00:13:16 -0500

    first commit

---
+++ src/main/modules/themes/builtInThemes.js
@@ -0,0 +1,40 @@
+const themes = {
+  classic: {
+    '--background': '#eee',
+    '--background-alt-1': '#e0e0e0',
+    '--background-alt-2': '#bbb',
+    '--primary': '#4c4c4c',
+    '--secondary': '#7c7c7c',
+    '--disabled': '#ccc',
+    '--button': '#4c4c4c',
+    '--button-text': '#eee',
+    '--button-disabled': '#ccc',
+    '--button-disabled-text': '#eee',
+    '--thumbnail-background': '#4c4c4c',
+    '--preview-background': '#4c4c4c',
+    '--preview-text': '#eee',
+    '--overlay-fade-in': 'rgba(238,238,238,0)',
+    '--overlay': 'rgba(238,238,238,0.5)',
+    '--record-light': '#ff4800'
+  },
+  dark: {
+    '--background': '#222',
+    '--background-alt-1': '#333',
+    '--background-alt-2': '#555',
+    '--primary': '#eee',
+    '--secondary': '#7c7c7c',
+    '--disabled': '#444',
+    '--button': '#4c4c4c', 
+    '--button-text': '#eee',
+    '--button-disabled': '#333',
+    '--button-disabled-text': '#444',
+    '--thumbnail-background': '#222',
+    '--preview-background': '#555',
+    '--preview-text': '#eee',
+    '--overlay-fade-in': 'rgba(68,68,68,0)',
+    '--overlay': 'rgba(68,68,68,0.75)',
+    '--record-light': '#ff4800'
+  }
+}
+
+export default themes
---
+++ src/main/modules/themes/loadTheme.js
@@ -0,0 +1,19 @@
+import styleToCSS from 'style-object-to-css-string'
+
+import builtInThemes from './builtInThemes'
+import { loadPrefs } from '../preferences/preferences'
+
+const cssKeyStore = new Map()
+
+export const loadTheme = async ({ webContents }, cssKeyName)  => {
+  let { theme } =  await loadPrefs()
+
+  if (!(theme in builtInThemes)) theme = 'classic'
+
+  const style = builtInThemes[theme]
+  const css = `:root{${styleToCSS(style)}}`
+
+  const key = await webContents.insertCSS(css)
+
+  cssKeyStore.set(cssKeyName, key)
+}
