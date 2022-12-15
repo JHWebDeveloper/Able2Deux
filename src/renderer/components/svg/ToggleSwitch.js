afabe1e9bf93daf09d5fa7ac14b585b7dc66ea41
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-15 10:31:55 -0500

    first path back to showing bg when toggled off

--- src/renderer/components/svg/ToggleSwitch.js
+++ src/renderer/components/svg/ToggleSwitch.js
@@ -3,7 +3,7 @@ import { bool } from 'prop-types'
 
 const ToggleSwitch = ({ toggle, disabled }) => (
 	<svg className={disabled ? 'toggle-switch-disabled' : ''} width="24" height="12" viewBox="0 0 24 12">
-		<path d="M18,0H6C2.688,0,0,2.688,0,6s2.688,6,6,6h12c3.312,0,6-2.688,6-6S21.312,0,18,0z"/>
+		<path fill={toggle ? '' : 'transparent'} d="M18,0H6C2.688,0,0,2.688,0,6s2.688,6,6,6h12c3.312,0,6-2.688,6-6S21.312,0,18,0z"/>
 		<path d="M18,0H6C2.688,0,0,2.688,0,6s2.688,6,6,6h12c3.312,0,6-2.688,6-6S21.312,0,18,0z M18,9.6H6
 			C4.008,9.6,2.4,7.992,2.4,6S4.008,2.4,6,2.4h11.995c0.002,0,0.003,0,0.005,0c1.992,0,3.6,1.608,3.6,3.6S19.992,9.6,18,9.6z"/>
 		{toggle ? <>
