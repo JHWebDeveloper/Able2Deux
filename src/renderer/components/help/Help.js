4a13f11a483e67b6a012a0f3094bd927452da858
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-16 00:41:11 -0500

    added message regarding dated help information

--- src/renderer/components/help/Help.js
+++ src/renderer/components/help/Help.js
@@ -27,6 +27,7 @@ const Help = () => {
 			{version ? <p>Able2 Version {version}</p> : <></>}
 			<p>Able2 is an all-in-one News Editor&apos;s video acquisition tool developed by Jonathan Hamilton and customized for the Editors and Photographers of WFTV in Orlando, FL. Able2 is able to download media from various online services, record video and audio from a user&apos;s desktop and convert and format video, image and audio files for air.</p>
 			<p>Report bugs to <address>able2@jonathanhamilton.com</address></p>
+			<p style={{ color: '#ff4800' }}>Please be advised that this help section is very out-of-date. Rewriting it is a bit too much for this lone developer to handle right now but an updated help section is on the to-do list for a future update. If you have any questions in the meantime, feel free to contact the email above.</p>
 		</header>
 		<TableOfContents />
 		<main>
