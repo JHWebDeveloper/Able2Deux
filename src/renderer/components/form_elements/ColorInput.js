f029fad59b9b2cc442268b9f929d9636a3e20fcd
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2022-12-21 00:37:52 -0500

    added props disabled and ariaLabelledBy

--- src/renderer/components/form_elements/ColorInput.js
+++ src/renderer/components/form_elements/ColorInput.js
@@ -3,7 +3,7 @@ import { bool, func, string } from 'prop-types'
 
 import { debounce } from 'utilities'
 
-const ColorInput = ({ name, value, title, onChange, onFocus, disabled }) => {
+const ColorInput = ({ name, value, title, onChange, onFocus, disabled, ariaLabelledby }) => {
 	const [ color, setColor ] = useState(value)
 
 	const onChangeDebounce = useMemo(() => debounce(onChange, 60), [onChange])
@@ -20,7 +20,8 @@ const ColorInput = ({ name, value, title, onChange, onFocus, disabled }) => {
       title={title}
 			onChange={e => setColor(e.target.value)}
 			onFocus={onFocus}
-      disabled={disabled} />
+      disabled={disabled}
+			aria-labelledby={ariaLabelledby} />
 	)
 }
 
