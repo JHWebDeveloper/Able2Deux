15bff6828d779322ab21023a17e1add6189dede4
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-01-01 00:11:53 -0500

    added props eyedropper and setEyedropper and reintroduced function getColorAtClickPos to support white and black balance in the new curves panel

--- src/renderer/components/formatting/preview/PreviewCanvas.js
+++ src/renderer/components/formatting/preview/PreviewCanvas.js
@@ -1,10 +1,27 @@
-import React, { useEffect, useMemo, useRef } from 'react'
-import { string } from 'prop-types'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
+import { bool, func, string } from 'prop-types'
 
-const PreviewCanvas = ({ previewStill }) => {
+const PreviewCanvas = ({ previewStill, eyedropper, setEyedropper }) => {
 	const cnv = useRef()
 	const ctx = useRef()
 	const img = useMemo(() => new Image(), [])
+
+	const getColorAtClickPos = useCallback(e => {
+		const { left, top } = cnv.current.getBoundingClientRect()
+		const x = e.clientX - left
+		const y = e.clientY - top
+		const [ r, g, b ] = ctx.current.getImageData(x, y, 1, 1).data
+
+		setEyedropper({
+			active: eyedropper.active,
+			pixelData: { r, g, b }
+		})
+	}, [cnv, ctx, eyedropper])
+
+	const eyedropperProps = useMemo(() => eyedropper.active ? {
+		className: 'eyedropper',
+		onClick: getColorAtClickPos
+	} : {}, [eyedropper.active, cnv, ctx])
 
 	useEffect(() => {
 		cnv.current.width = 384
@@ -22,12 +39,17 @@ const PreviewCanvas = ({ previewStill }) => {
 	}, [previewStill])
 
 	return (
-		<canvas ref={cnv} ></canvas>
+		<canvas
+			ref={cnv}
+			className={eyedropper.active ? 'eyedropper' : ''}
+			{...eyedropperProps}></canvas>
 	)
 }
 
 PreviewCanvas.propTypes = {
-	previewStill: string.isRequired
+	previewStill: string.isRequired,
+	eyedropper: bool.isRequired,
+	setEyedropper: func.isRequired
 }
 
 export default PreviewCanvas
