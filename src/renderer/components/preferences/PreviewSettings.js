9def2c7f98e38dc1c32d6fe79c326ab038a4a9e1
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-01-02 14:46:13 -0500

    added pref Default Quality

--- src/renderer/components/preferences/PreviewSettings.js
+++ src/renderer/components/preferences/PreviewSettings.js
@@ -1,16 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext } from 'react'
 
 import { PrefsContext } from 'store/preferences'
 
-import { updateStateFromEvent } from 'actions'
+import { updateState, updateStateFromEvent } from 'actions'
 
+import RadioSet from '../form_elements/RadioSet'
 import AspectRatioMarkers from './AspectRatioMarkers'
+
+const qualityButtons = [
+	{
+		label: '100%',
+		value: '1'
+	},
+	{
+		label: '50%',
+		value: '2'
+	},
+	{
+		label: '25%',
+		value: '4'
+	}
+]
 
 const PreviewSettings = () => {
 	const { preferences, dispatch } = useContext(PrefsContext)
 
+	const setPreviewQuality = useCallback(e => {
+		dispatch(updateState({
+			previewQuality: parseInt(e.target.value)
+		}))
+	}, [])
+
 	return (
 		<form>
+			<fieldset>
+				<legend>Default Quality:</legend>
+				<RadioSet
+					name="previewQuality"
+					buttons={qualityButtons}
+					state={preferences.previewQuality.toString()}
+					onChange={setPreviewQuality} />
+			</fieldset>
 			<span className="input-option">
 				<label htmlFor="grid-color">Grid Color</label>
 				<input
