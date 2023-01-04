aa0eec80b819ae770fc0daa010a7209d0570eece
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-01-04 10:34:22 -0500

    quality prop type not accepts 0.5, 0.75 or 1

--- src/renderer/components/svg/QualityIcon.js
+++ src/renderer/components/svg/QualityIcon.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { number } from 'prop-types'
+import { oneOf } from 'prop-types'
 
 const QualityIconPath = ({ quality }) => {
 	switch (quality) {
 		case 1:
 			return <path d="M14.4,0H3.6C1.6,0,0,1.6,0,3.6V9c0,2,1.6,3.6,3.6,3.6h10.8c2,0,3.6-1.6,3.6-3.6V3.6C18,1.6,16.4,0,14.4,0L14.4,0z"/>
-		case 2:
+		case 0.75:
 			return <path d="M14.4,0H3.6C1.6,0,0,1.6,0,3.6V9c0,2,1.6,3.6,3.6,3.6h10.8c2,0,3.6-1.6,3.6-3.6V3.6C18,1.6,16.4,0,14.4,0z M17.1,9c0,1.5-1.2,2.7-2.7,2.7H5.9l6.2-10.8h2.3c1.5,0,2.7,1.2,2.7,2.7V9z"/>
-		case 4:
+		case 0.5:
 			return <path d="M14.4,0.9c1.5,0,2.7,1.2,2.7,2.7V9c0,1.5-1.2,2.7-2.7,2.7H3.6c-1.5,0-2.7-1.2-2.7-2.7V3.6c0-1.5,1.2-2.7,2.7-2.7L14.4,0.9M14.4,0H3.6C1.6,0,0,1.6,0,3.6V9c0,2,1.6,3.6,3.6,3.6h10.8c2,0,3.6-1.6,3.6-3.6V3.6C18,1.6,16.4,0,14.4,0L14.4,0z"/>
 		default:
 			return <></>
@@ -20,7 +20,7 @@ const QualityIcon = ({ quality }) => (
 	</svg>
 )
 
-QualityIconPath.propTypes = { quality: number.isRequired }
-QualityIcon.propTypes = { quality: number.isRequired }
+QualityIconPath.propTypes = { quality: oneOf([1, 0.75, 0.5]).isRequired }
+QualityIcon.propTypes = { quality: oneOf([1, 0.75, 0.5]).isRequired }
 
 export default QualityIcon
