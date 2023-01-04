c1ff373bdb98780b45aa2c26e9dee86b23a2b2d6
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-01-04 14:30:27 -0500

    imported uuid, rearranged functions and added category heading comments

--- src/renderer/utilities/valueModifiers.js
+++ src/renderer/utilities/valueModifiers.js
@@ -1,14 +1,10 @@
+import { v1 as uuid } from 'uuid'
+
 import { getIntegerLength } from '.'
 
 export const capitalize = str => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`
 
-const getRegex = asperaSafe => new RegExp(`([%&"/:;<>?\\\\\`${asperaSafe ? '|ŒœŠšŸ​]|[^!-ż\\s' : ''}])`, 'g')
-
-export const cleanFilename = (fileName, asperaSafe) => fileName
-	.replace(getRegex(asperaSafe), '_')
-	.trim()
-	.slice(0, 252)
-	.trimEnd()
+// ---- curve arrays ---- //
 
 export const copyCurve = curve => curve.map(pt => ({ ...pt, id: uuid() }))
 
@@ -25,38 +21,15 @@ export const copyCurveSet = curves => {
 
 export const sortCurvePoints = (a, b) => a.x - b.x
 
+// ---- zero padders ---- //
+
 export const zeroizeAuto = (n, total) => zeroize(n, getIntegerLength(total))
 
 export const zeroize = (n, zeroes = 2) => n
 	.toString()
 	.padStart(zeroes, '0')
 
-export const replaceTokens = (filename, i = 0, l = 0) => {
-	const d = new Date()
-
-	return filename
-		.replace(/\$n/g, zeroizeAuto(i + 1, l))
-		.replace(/\$r/g, zeroizeAuto(l - i, l))
-		.replace(/\$b/g, l)
-		.replace(/\$d/g, d.toDateString())
-		.replace(/\$D/g, d.toLocaleDateString().replace(/\//g, '-'))
-		.replace(/\$t/g, format12hr(d))
-		.replace(/\$T/g, `${d.getHours()}${d.getMinutes()}`)
-}
-
-export const format12hr = d => {
-	let h = d.getHours()
-	const m = d.getMinutes()
-	const meridian = h < 12 ? 'am' : 'pm'
-
-	if (h > 12) {
-		h -= 12
-	} else if (h === 0) {
-		h = 12
-	}
-
-	return `${h}${zeroize(m)}${meridian}`
-}
+// ---- timecodes and timestamps ---- //
 
 export const secondsToTC = sec => [
 	sec / 3600,
@@ -92,4 +65,41 @@ export const tcToFrames = (hmsf, fps) => {
 	if (parts[3]) frms += parts[3]
 
 	return frms
+}
+
+export const format12hr = d => {
+	let h = d.getHours()
+	const m = d.getMinutes()
+	const meridian = h < 12 ? 'am' : 'pm'
+
+	if (h > 12) {
+		h -= 12
+	} else if (h === 0) {
+		h = 12
+	}
+
+	return `${h}${zeroize(m)}${meridian}`
+}
+
+// ---- filenames ---- //
+
+const getRegex = asperaSafe => new RegExp(`([%&"/:;<>?\\\\\`${asperaSafe ? '|ŒœŠšŸ​]|[^!-ż\\s' : ''}])`, 'g')
+
+export const cleanFilename = (fileName, asperaSafe) => fileName
+	.replace(getRegex(asperaSafe), '_')
+	.trim()
+	.slice(0, 252)
+	.trimEnd()
+
+export const replaceTokens = (filename, i = 0, l = 0) => {
+	const d = new Date()
+
+	return filename
+		.replace(/\$n/g, zeroizeAuto(i + 1, l))
+		.replace(/\$r/g, zeroizeAuto(l - i, l))
+		.replace(/\$b/g, l)
+		.replace(/\$d/g, d.toDateString())
+		.replace(/\$D/g, d.toLocaleDateString().replace(/\//g, '-'))
+		.replace(/\$t/g, format12hr(d))
+		.replace(/\$T/g, `${d.getHours()}${d.getMinutes()}`)
 }
