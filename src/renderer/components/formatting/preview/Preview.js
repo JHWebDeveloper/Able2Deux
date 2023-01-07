735b210f474b60713ec1a7110780d7cd5ccf02cd
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-01-07 03:06:26 -0500

    removed unused variables

--- src/renderer/components/formatting/preview/Preview.js
+++ src/renderer/components/formatting/preview/Preview.js
@@ -22,9 +22,7 @@ const Preview = ({ selected, eyedropper, setEyedropper, aspectRatioMarkers, prev
 	const {
 		id,
 		mediaType,
-		duration,
 		aspectRatio,
-		fps,
 		audio,
 		arc,
 		background,
