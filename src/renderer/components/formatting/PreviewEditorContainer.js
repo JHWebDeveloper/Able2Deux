c5bbf77b29a80096ed91d29ecd3f8444aee925e8
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-01-10 13:35:41 -0500

    added function resetEyedropperOnEscape with useEffect to bind and unbind to keydown listener

--- src/renderer/components/formatting/PreviewEditorContainer.js
+++ src/renderer/components/formatting/PreviewEditorContainer.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { arrayOf, bool, exact, func, number, object, oneOf, string } from 'prop-types'
 
 import PreviewContainer from './preview/PreviewContainer'
 import EditorOptions from './editor/EditorOptions'
 
-const PreviewEditorContainer = ({ selected, aspectRatioMarkers, previewQuality, editAll, batch, split, isBatch, dispatch }) => {
-	const [ eyedropper, setEyedropper ] = useState({ active: false, pixelData: false })
+const eyeDropperInit = { active: false, pixelData: false }
+
+const PreviewEditorContainer = props => {
+	const [ eyedropper, setEyedropper ] = useState(eyeDropperInit)
+	const { selected, dispatch } = props
+
+	const resetEyedropperOnEscape = useCallback(e => {
+		if (e.key === 'Escape') setEyedropper(eyeDropperInit)
+	}, [])
+
+	useEffect(() => {
+		window.addEventListener('keydown', resetEyedropperOnEscape)
+
+		return () => {
+			window.removeEventListener('keydown', resetEyedropperOnEscape)
+		}
+	}, [])
 
 	return (
 		<div id="editor">
@@ -13,16 +28,16 @@ const PreviewEditorContainer = ({ selected, aspectRatioMarkers, previewQuality, 
 				eyedropper={eyedropper}
 				setEyedropper={setEyedropper}
 				selected={selected}
-				aspectRatioMarkers={aspectRatioMarkers}
-				previewQuality={previewQuality}
+				aspectRatioMarkers={props.aspectRatioMarkers}
+				previewQuality={props.previewQuality}
 				dispatch={dispatch} />
 			<EditorOptions
 				eyedropper={eyedropper}
 				setEyedropper={setEyedropper}
-				batch={batch}
-				editAll={editAll}
-				split={split}
-				isBatch={isBatch}
+				batch={props.batch}
+				editAll={props.editAll}
+				split={props.split}
+				isBatch={props.isBatch}
 				dispatch={dispatch}
 				{...selected} />
 		</div>
