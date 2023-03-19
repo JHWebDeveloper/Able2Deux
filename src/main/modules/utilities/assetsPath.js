da205378e99324641767e0c391a2850527122021
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-03-19 01:46:59 -0400

    consolidated smaller utilities into index.js and deleted files

--- src/main/modules/utilities/assetsPath.js
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-import { fixPathForAsarUnpack } from 'electron-util'
-
-export const assetsPath = fixPathForAsarUnpack(process.env.NODE_ENV === 'development'
-	? path.join(__dirname, '..', '..', 'backgrounds')
-	: path.join(__dirname, 'assets', 'backgrounds'))
--- src/main/modules/utilities/index.js
+++ src/main/modules/utilities/index.js
@@ -1,7 +1,51 @@
-export * from './assetsPath'
-export * from './base64Encode'
-export * from './fileExistsPromise'
-export * from './getOverlayInnerDimensions'
-export * from './placeholder'
+import { promises as fsp } from 'fs'
+import path from 'path'
+import { fixPathForAsarUnpack } from 'electron-util'
+
+import { placeholder } from './placeholder'
+
 export * from './fileSupportLists'
 export * from '../../../shared/utilities'
+
+export const assetsPath = fixPathForAsarUnpack(process.env.NODE_ENV === 'development'
+	? path.join(__dirname, '..', '..', 'backgrounds')
+	: path.join(__dirname, 'assets', 'backgrounds'))
+
+export const base64Encode = async file => `data:image/png;base64,${await fsp.readFile(file, 'base64')}`
+
+export const base64EncodeOrPlaceholder = async file => {
+  if (!file) return placeholder
+
+  try {
+    return base64Encode(file)
+  } catch (err) {
+    console.error(err)
+    return placeholder
+  }
+}
+
+export const fileExistsPromise = async fileOrDir => {
+	try {
+		await fsp.access(fileOrDir)
+		return true
+	} catch (err) {
+		return false
+	}
+}
+
+export const getOverlayInnerDimensions = (size, overlay) => {
+	const is1080 = size === '1080'
+
+	return {
+		tv: {
+			width: is1080 ? 1576 : 1050,
+			height: is1080 ? 886 : 590,
+			y: is1080 ? 78 : 52
+		},
+		laptop: {
+			width: is1080 ? 1366 : 912,
+			height: is1080 ? 778 : 518,
+			y: is1080 ? 86 : 58
+		}
+	}[overlay]
+}
