7df53f08275fd326ca6551add31f4ce6fe9fc620
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-04-05 18:38:13 +0900

    fixed lint errors

--- src/renderer/hooks/useAugmentedDispatch.js
+++ src/renderer/hooks/useAugmentedDispatch.js
@@ -1,9 +1,9 @@
 import { useReducer } from 'react'
 
 export const useAugmentedDispatch = (reducer, initState) => {
-  const [ state, dispatch ] = useReducer(reducer, initState)
+	const [ state, dispatch ] = useReducer(reducer, initState)
 
-  const augmentedDispatch = input => input instanceof Function ? input(dispatch, state) : dispatch(input)
+	const augmentedDispatch = input => input instanceof Function ? input(dispatch, state) : dispatch(input)
 
-  return [ state, augmentedDispatch ]
+	return [ state, augmentedDispatch ]
 }
--- src/renderer/hooks/useToggle.js
+++ src/renderer/hooks/useToggle.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 export const useToggle = (initState = false) => {
-  const [ value, setValue ] = useState(initState)
+	const [ value, setValue ] = useState(initState)
 
-  const toggleValue = newValue => {
-    setValue(currentValue => (
-      typeof newValue === 'boolean' ? newValue : !currentValue
-    ))
-  }
+	const toggleValue = newValue => {
+		setValue(currentValue => (
+			typeof newValue === 'boolean' ? newValue : !currentValue
+		))
+	}
 
-  return [ value, toggleValue ]
+	return [ value, toggleValue ]
 }
