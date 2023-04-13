e4a15d04490400047cd13fbb3acc1f16b057ff24
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-04-13 08:26:36 -0400

    removed join methods

--- src/main/modules/formatting/filters/fitFilter.js
+++ src/main/modules/formatting/filters/fitFilter.js
@@ -7,17 +7,14 @@ import {
 } from '.'
 
 const cmdChunks = [
-	':force_original_aspect_ratio=decrease[fg];',
+	':force_original_aspect_ratio=decrease[fg];[',
 	':v][fg]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2'
 ]
 
 export const fit = (filterData, isPreview, previewSize) => {
 	const { keying, rotation, colorCurves, sourceData, overlayDim, renderWidth, renderHeight } = filterData
 
-	const filter = [
-		`[0:v]${buildKeyFilter(isPreview, keying)}${buildCommonFilter(isPreview, rotation, colorCurves)},scale=w=${renderWidth}:h=${renderHeight}${cmdChunks[0]}`,
-		`[${getBGLayerNumber(sourceData, overlayDim)}${cmdChunks[1]}${shortestAndFormat}`
-	].join('')
+	const filter = `[0:v]${buildKeyFilter(isPreview, keying)}${buildCommonFilter(isPreview, rotation, colorCurves)},scale=w=${renderWidth}:h=${renderHeight}${cmdChunks[0]}${getBGLayerNumber(sourceData, overlayDim)}${cmdChunks[1]}${shortestAndFormat}`
 
 	return finalize({ filter, sourceData, overlayDim, isPreview, previewSize })
 }
