0a25899390b69c83c7c94c1d12199c849478f492
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-04-15 11:43:12 -0400

    moved freeRotateFilter to variable freeRotate to help reduce length of template literal filter.

--- src/main/modules/formatting/filters/transformFilter.js
+++ src/main/modules/formatting/filters/transformFilter.js
@@ -29,7 +29,8 @@ export const transform = (filterData, isPreview, previewSize) => {
 	position.x /= 100
 	position.y /= 100
 
-	const filter = `[0:v]${buildKeyFilter(isPreview, keying)}${buildCommonFilter(isPreview, rotation, colorCurves)},scale=${scale.x || 0.005}*iw:${scale.y || 0.005}*ih,crop=${cropW}*iw:${cropH}*ih:${crop.l}*iw:${crop.t}${cmdChunks[0]}${angle === 0 ? '' : freeRotateFilter(rotation, width * scale.x * cropW, height * scale.y * cropH)}[fg];[${getBGLayerNumber(sourceData, overlayDim)}${cmdChunks[1]}${position.x}${cmdChunks[2]}${position.y}${cmdChunks[3]}${shortestAndFormat}`
+	const freeRotate = angle === 0 ? '' : freeRotateFilter(rotation, width * scale.x * cropW, height * scale.y * cropH)
+	const filter = `[0:v]${buildKeyFilter(isPreview, keying)}${buildCommonFilter(isPreview, rotation, colorCurves)},scale=${scale.x || 0.005}*iw:${scale.y || 0.005}*ih,crop=${cropW}*iw:${cropH}*ih:${crop.l}*iw:${crop.t}${cmdChunks[0]}${freeRotate}[fg];[${getBGLayerNumber(sourceData, overlayDim)}${cmdChunks[1]}${position.x}${cmdChunks[2]}${position.y}${cmdChunks[3]}${shortestAndFormat}`
 
 	return finalize({ filter, sourceData, overlayDim, isPreview, previewSize })
 }
