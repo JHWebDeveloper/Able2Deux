21ea3cdf20e9c5ddc2d96c570ff6e42e3d312b28
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-07-16 22:47:24 -0400

    passing state as second parameter to dispatch when param input is function

--- src/renderer/hooks/useAugmentedDispatch.js
+++ src/renderer/hooks/useAugmentedDispatch.js
@@ -3,7 +3,7 @@ import { useReducer } from 'react'
 export const useAugmentedDispatch = (reducer, initState) => {
 	const [ state, dispatch ] = useReducer(reducer, initState)
 
-	const augmentedDispatch = input => input instanceof Function ? input(dispatch) : dispatch(input)
+	const augmentedDispatch = input => input instanceof Function ? input(dispatch, state) : dispatch(input)
 
 	return [ state, augmentedDispatch ]
 }
