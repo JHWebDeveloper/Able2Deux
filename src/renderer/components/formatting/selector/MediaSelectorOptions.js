490a8c0cda37c5e9b095f1904573d551408ba128
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-07-19 00:28:12 -0400

    moved dropdown to parent

--- src/renderer/components/formatting/selector/MediaSelectorOptions.js
+++ src/renderer/components/formatting/selector/MediaSelectorOptions.js
@@ -1,120 +1,39 @@
-import React, { useCallback, useContext, useMemo } from 'react'
-import { arrayOf, bool, func, object } from 'prop-types'
-
-import { PrefsContext } from 'store'
-
-import {
-	disableWarningAndSave,
-	deselectAllMedia,
-	duplicateSelectedMedia,
-	selectAllMedia,
-	removeAllMedia,
-	removeSelectedMedia
-} from 'actions'
-
-import { warn } from 'utilities'
+import React from 'react'
+import { arrayOf, bool, func, oneOf, shape, string } from 'prop-types'
 
 import DropdownMenu from '../../form_elements/DropdownMenu.js'
 import MediaOptionButtons from '../../form_elements/MediaOptionButtons'
 
-const { interop } = window.ABLE2
-
-const ctrlOrCmdKeySymbol = interop.isMac ? '⌘' : '⌃'
-
-const MediaSelectorOptions = ({ media, allItemsSelected, multipleItemsSelected, dispatch }) => {
-	const { preferences, dispatch: dispatchPrefs } = useContext(PrefsContext)
-	const { warnings } = preferences
-
-	const dispatchSelectAllMedia = useCallback(() => {
-		dispatch(selectAllMedia())
-	}, [])
-
-	const dispatchDeselectAllMedia = useCallback(() => {
-		dispatch(deselectAllMedia())
-	}, [])
-
-	const removeMediaWarning = useCallback(({ message, action }) => warn({
-		message,
-		detail: 'This cannot be undone. Proceed?',
-		enabled: warnings.removeAll,
-		callback() {
-			dispatch(action())
-		},
-		checkboxCallback() {
-			dispatchPrefs(disableWarningAndSave('removeAll'))
-		}
-	}), [media, warnings.removeAll])
-
-	const settingsMenu = useMemo(() => [
-		{
-			label: 'Select All',
-			hide: allItemsSelected,
-			shortcut: `${ctrlOrCmdKeySymbol}A`,
-			action: dispatchSelectAllMedia
-		},
-		{
-			label: 'Deselect All',
-			hide: !multipleItemsSelected,
-			shortcut: `⇧${ctrlOrCmdKeySymbol}A`,
-			action: dispatchDeselectAllMedia
-		},
-		{ type: 'spacer' },
-		{
-			label: 'Duplicate Selected',
-			hide: !multipleItemsSelected,
-			action() {
-				dispatch(duplicateSelectedMedia())
-			}
-		},
-		{ type: 'spacer' },
-		{
-			label: 'Remove Selected',
-			hide: !multipleItemsSelected || allItemsSelected,
-			action() {
-				dispatch(removeMediaWarning({
-					message: 'Remove Selected Media?',
-					action: removeSelectedMedia
-				}))
-			}
-		},
-		{
-			label: 'Remove All',
-			action() {
-				dispatch(removeMediaWarning({
-					message: 'Remove All Media?',
-					action: removeAllMedia
-				}))
-			}
-		}
-	], [media, allItemsSelected, multipleItemsSelected])
-
-	return (
-		<div>
-			<DropdownMenu>
-				<MediaOptionButtons buttons={settingsMenu} />
-			</DropdownMenu>
-			{allItemsSelected ? (
-				<button
-					type="button"
-					name="deselectAll"
-					className="app-button"
-					onClick={dispatchDeselectAllMedia}>Deselect All</button>
-			) : (
-				<button
-					type="button"
-					name="selectAll"
-					className="app-button"
-					onClick={dispatchSelectAllMedia}>Select All</button>
-			)}
-		</div>
-	)
-}
+const MediaSelectorOptions = ({ allItemsSelected, dropdown }) => (
+	<div>
+		<DropdownMenu>
+			<MediaOptionButtons buttons={dropdown} />
+		</DropdownMenu>
+		{allItemsSelected ? (
+			<button
+				type="button"
+				name="deselectAll"
+				className="app-button"
+				onClick={dropdown[1].action}>Deselect All</button>
+		) : (
+			<button
+				type="button"
+				name="selectAll"
+				className="app-button"
+				onClick={dropdown[0].action}>Select All</button>
+		)}
+	</div>
+)
 
 MediaSelectorOptions.propTypes = {
-	media: arrayOf(object).isRequired,
-	multipleItemsSelected: bool.isRequired,
 	allItemsSelected: bool.isRequired,
-	dispatch: func.isRequired
+	dropdown: arrayOf(shape({
+		type: string,
+		label: string,
+		hide: bool,
+		shortcut: string,
+		action: func
+	})).isRequired
 }
 
 export default MediaSelectorOptions
