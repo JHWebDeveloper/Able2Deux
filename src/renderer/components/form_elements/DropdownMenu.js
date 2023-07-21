c91ebf5b520534b8d24cfbf8f216c1189aea20fb
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-07-21 11:59:22 -0400

    added functions navigateWithKeys and getFocusableSibling and passed to cloned child element. This will allow navigating a dropdown menu with arrow keys and closing it with escape key.

--- src/renderer/components/form_elements/DropdownMenu.js
+++ src/renderer/components/form_elements/DropdownMenu.js
@@ -1,13 +1,19 @@
-import React, { cloneElement, useCallback, useId, useState } from 'react'
+import React, { cloneElement, useCallback, useId, useRef, useState } from 'react'
 import { arrayOf, element, oneOfType, string } from 'prop-types'
 
 import { useToggle } from 'hooks'
 import { detectTabExit } from 'utilities'
 
+const getFocusableSibling = (el, prop) => {
+	const sibling = el[prop]
+
+	return sibling?.tabIndex === -1 ? getFocusableSibling(sibling, dir) : sibling
+}
+
 const DropdownMenu = ({ icon = 'more_vert', children }) => {
 	const [ menu, toggleMenu ] = useToggle()
 	const [ position, setPosition ] = useState({ top: 0, left: 0 })
-
+	const toggleMenuButton = useRef(null)
 	const menuRefId = useId()
 
 	const getPositionRelativeToWindow = useCallback(e => {
@@ -23,9 +29,23 @@ const DropdownMenu = ({ icon = 'more_vert', children }) => {
 
 	const closeMenuOnBlur = useCallback(detectTabExit(toggleMenu), [])
 
+	const navigateWithKeys = useCallback(e => {
+		e.stopPropagation()
+		
+		if (e.key === 'Escape') {
+			toggleMenu()
+			toggleMenuButton.current.focus()
+		} else if (e.key === 'ArrowUp' || e.key === 'ArrowLeft') {
+			getFocusableSibling(e.currentTarget, 'previousElementSibling')?.focus()
+		} else if (e.key === 'ArrowDown' || e.key === 'ArrowRight') {
+			getFocusableSibling(e.currentTarget, 'nextElementSibling')?.focus()
+		}
+	}, [])
+
 	return (
 		<span className="dropdown" onBlur={closeMenuOnBlur}>
 			<button
+				ref={toggleMenuButton}
 				type="button"
 				title="Options"
 				className="symbol"
@@ -40,7 +60,7 @@ const DropdownMenu = ({ icon = 'more_vert', children }) => {
 					aria-label="Options"
 					id={menuRefId}
 					style={position}>
-					{cloneElement(children, { toggleMenu })}
+					{cloneElement(children, { toggleMenu, navigateWithKeys })}
 				</span>
 			) : <></>}
 		</span>
