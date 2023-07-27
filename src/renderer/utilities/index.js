ff961f387c7423f03e278bfc6739bee4d5919e04
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-07-27 17:10:39 -0400

    added regex string validURLRegex to constants section

--- src/renderer/utilities/index.js
+++ src/renderer/utilities/index.js
@@ -19,6 +19,8 @@ export const toastrOpts = {
 	extendedTimeOut: 0
 }
 
+export const validURLRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+
 // ---- CALCULATORS/CONVERTERS --------
 
 export const clamp = (val, min, max) => Math.max(min, Math.min(max, val))
