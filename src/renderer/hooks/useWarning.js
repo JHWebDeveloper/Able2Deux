1caee033e30d9f822f7e71aa7736f14d69d41c08
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-07-30 00:01:49 -0400

    added new customHook useSaveWarning

--- src/renderer/hooks/useWarning.js
+++ src/renderer/hooks/useWarning.js
@@ -3,6 +3,8 @@ import { useCallback, useContext } from 'react'
 import { PrefsContext } from 'store'
 import { disableWarningAndSave } from 'actions'
 
+const { warning } = window.ABLE2.interop
+
 export const useWarning = ({
 	name: warningName,
 	message: fixedMessage,
@@ -25,21 +27,49 @@ export const useWarning = ({
 		skip = fixedSkip
 	} = {}) => {
 		if (!skip && (!warningName || warnings[warningName])) {
-			const { response, checkboxChecked } = await window.ABLE2.interop.warning({
+			const { response, checkboxChecked } = await warning({
 				message,
 				detail,
 				hasCheckbox
 			})
 	
-			if (response > 0) return false
-	
+			if (response > 0) return
 			if (hasCheckbox && checkboxChecked) disableWarning()
 		}
 	
 		callback()
-
-		return true
 	}, [warnings, warningName, fixedSkip, fixedMessage, fixedDetail, fixedCallback, hasCheckbox, ...dependencies])
+
+	return warn
+}
+
+export const useSaveWarning = ({
+	message: fixedMessage = 'You Have Unsaved Changes!',
+	detail: fixedDetail,
+	onConfirm: fixedOnConfirm,
+	onSave: fixedOnSave,
+	skip: fixedSkip
+}, dependencies = []) => {
+	const warn = useCallback(async ({
+		message = fixedMessage,
+		detail = fixedDetail,
+		onConfirm = fixedOnConfirm,
+		onSave = fixedOnSave,
+		skip = fixedSkip
+	} = {}) => {
+		if (!skip) {
+			const { response } = await warning({
+				message,
+				detail,
+				buttons: ['Close', 'Save & Close', 'Cancel']
+			})
+
+			if (response > 1) return
+			if (response === 1) return onSave()
+		}
+
+		onConfirm()
+	}, [fixedSkip, fixedMessage, fixedDetail, fixedOnConfirm, fixedOnSave, ...dependencies])
 
 	return warn
 }
