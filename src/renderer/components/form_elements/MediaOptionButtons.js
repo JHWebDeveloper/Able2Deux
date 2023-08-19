f53a893d58dd3ce790fcf7675d4acf8bf66e1ad4
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-08-19 12:40:02 -0400

    added a condition checking for existence of submenu property in buttons list to return a nested DropdownMenu component as a submenu

--- src/renderer/components/form_elements/MediaOptionButtons.js
+++ src/renderer/components/form_elements/MediaOptionButtons.js
@@ -1,33 +1,66 @@
 import React, { useId, useMemo } from 'react'
+import { arrayOf, bool, func, shape, string } from 'prop-types'
 
-const MediaOptionButtons = ({ buttons, toggleMenu, navigateWithKeys }) => {
+import DropdownMenu from './DropdownMenu'
+
+const MediaOptionButtons = ({ buttons, navigateWithKeys, parentMenu }) => {
 	const menuId = useId()
 	const enabledButtons = useMemo(() => buttons.filter(({ hide }) => !hide), [buttons])
 
-	return enabledButtons.map(({ type, label, action, shortcut }, i) => type === 'spacer' ? (
-		<span
-			key={`${menuId}_${i}`}
-			className="spacer"
-			aria-hidden
-			data-no-drag></span>
-	) : (
-		<button
-			key={`${menuId}_${i}`}
-			type="button"
-			role="menuitem"
-			title={label}
-			aria-label={label}
-			autoFocus={i === 0}
-			onClick={() => {
-				action()
-				toggleMenu(false)
-			}}
-			onKeyDown={navigateWithKeys}
-			data-no-drag>
-			<span>{label}</span>
-			{shortcut ? <kbd>{shortcut}</kbd> : <></>}
-		</button>
-	))
+	return enabledButtons.map((props, i) => {
+		const { type, label, action, shortcut } = props
+
+		if (type === 'spacer') {
+			return (
+				<span
+					key={`${menuId}_${i}`}
+					className="spacer"
+					aria-hidden
+					data-no-drag></span>
+			)
+		} else if ('submenu' in props) {
+			return (
+				<DropdownMenu
+					key={`${menuId}_${i}`}
+					label={label}
+					parentMenu={parentMenu}
+					submenu>
+					<MediaOptionButtons buttons={props.submenu} />
+				</DropdownMenu>
+			)
+		} else {
+			return (
+				<button
+					key={`${menuId}_${i}`}
+					type="button"
+					role="menuitem"
+					title={label}
+					aria-label={label}
+					autoFocus={i === 0}
+					onClick={action}
+					onKeyDown={navigateWithKeys}
+					data-no-drag>
+					<span>{label}</span>
+					{shortcut ? <kbd>{shortcut}</kbd> : <></>}
+				</button>
+			)
+		}
+	})
+}
+
+const buttonPropType = shape({
+	type: string,
+	label: string,
+	hide: bool,
+	shortcut: string,
+	action: func,
+})
+
+buttonPropType.submenu = arrayOf(buttonPropType)
+
+MediaOptionButtons.propTypes = {
+	buttons: arrayOf(buttonPropType),
+	navigateWithKeys: func
 }
 
 export default MediaOptionButtons
