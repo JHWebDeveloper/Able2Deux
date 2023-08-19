d49183933d73c5ac399c63a6efd433f6ecc8cb2b
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-08-19 12:38:35 -0400

    reconfigured component DropdownMenu to allow for infinite nesting of menus inside of menus for a submenu system. Allowed optional icon or text for dropdown button.

--- src/renderer/components/form_elements/DropdownMenu.js
+++ src/renderer/components/form_elements/DropdownMenu.js
@@ -1,8 +1,11 @@
-import React, { cloneElement, useCallback, useId, useRef, useState } from 'react'
-import { arrayOf, element, oneOfType, string } from 'prop-types'
+import React, { cloneElement, useCallback, useEffect, useId, useMemo, useRef, useState } from 'react'
+import { arrayOf, bool, element, oneOfType, shape, string } from 'prop-types'
 
-import { useToggle } from 'hooks'
-import { detectTabExit, isArrowNext, isArrowPrev } from 'utilities'
+import {
+	detectTabExit,
+	isArrowNext,
+	isArrowPrev
+} from 'utilities'
 
 const getFocusableSibling = (el, prop) => {
 	const sibling = el[prop]
@@ -10,57 +13,140 @@ const getFocusableSibling = (el, prop) => {
 	return sibling?.tabIndex === -1 ? getFocusableSibling(sibling, prop) : sibling
 }
 
-const DropdownMenu = ({ icon = 'more_vert', children }) => {
-	const [ showMenu, toggleShowMenu ] = useToggle()
+const stopPropagation = e => e.stopPropagation()
+
+const DropdownMenu = ({
+	submenu = false,
+	label = 'Options',
+	icon,
+	parentMenu,
+	children
+}) => {
+	const [ showMenu, setShowMenu ] = useState(false)
 	const [ position, setPosition ] = useState({ top: 0, left: 0 })
-	const toggleMenuButton = useRef(null)
-	const menuRefId = useId()
+	const menuButton = useRef(null)
+	const menuId = useId()
 
 	const getPositionRelativeToWindow = useCallback(e => {
-		const { bottom, left } = e.target.getBoundingClientRect()
+		const rect = e.currentTarget.children[0].getBoundingClientRect()
 
-		setPosition({
-			top: `${bottom + 3}px`,
-			left: `${left}px`
-		})
+		if (submenu) {
+			const { top, left, width } = rect
 
-		toggleShowMenu()
+			setPosition({
+				top: `${top - 11}px`,
+				left: `${left + width + 6}px`
+			})
+		} else {
+			const { bottom, left } = rect
+
+			setPosition({
+				top: `${bottom}px`,
+				left: `${left}px`
+			})
+		}
+
+		setShowMenu(true)
 	}, [])
 
-	const closeMenuOnBlur = useCallback(detectTabExit(toggleShowMenu), [])
+	const closeCurrentMenu = useCallback(() => {
+		setShowMenu(false)
+	}, [showMenu])
+
+	const toggleMenu = useCallback(e => {
+		if (showMenu) {
+			closeCurrentMenu()
+		} else {
+			getPositionRelativeToWindow(e)
+		}
+	}, [showMenu])
+
+	const closeMenuOnBlur = useCallback(detectTabExit(closeCurrentMenu), [])
 
 	const navigateWithKeys = useCallback(e => {
-		if (e.key !== 'Tab') e.stopPropagation()
-		
-		if (e.key === 'Escape') {
-			toggleShowMenu()
-			toggleMenuButton.current.focus()
-		} else if (isArrowPrev(e)) {
+		if (e.key !== 'Tab' && e.key !== 'Escape') e.stopPropagation()
+
+		if (isArrowPrev(e)) {
 			getFocusableSibling(e.currentTarget, 'previousElementSibling')?.focus()
 		} else if (isArrowNext(e)) {
 			getFocusableSibling(e.currentTarget, 'nextElementSibling')?.focus()
 		}
-	}, [])
+	}, [showMenu])
+
+	const rootMenuOrSubmenuProps = useMemo(() => submenu ? {
+		role: 'menuitem button',
+		onClick: stopPropagation,
+		onMouseEnter: getPositionRelativeToWindow,
+		onMouseLeave() {
+			closeCurrentMenu()
+			menuButton.current.focus()
+		},
+		onKeyDown(e) {
+			if (e.key === 'Enter') {
+				e.preventDefault()
+				e.stopPropagation()
+				toggleMenu(e)
+			} else {
+				navigateWithKeys(e)
+			}
+		}
+	} : {
+		onClick: toggleMenu,
+		onKeyDown(e) {
+			if (e.key === 'Escape') {
+				closeCurrentMenu()
+				menuButton.current.focus()
+			} else if (e.key === 'Enter') {
+				e.preventDefault()
+				toggleMenu(e)
+			}
+		}
+	}, [showMenu])
+
+	useEffect(() => {
+		const shouldListen = showMenu && parentMenu?.current && menuButton?.current
+
+		if (!shouldListen) return
+
+		const closeOnParentMouseOver = e => {
+			if (!menuButton.current.contains(e.target)) closeCurrentMenu()
+		}
+
+		parentMenu.current.addEventListener('mouseover', closeOnParentMouseOver)
+
+		return () => {
+			if (shouldListen) {
+				parentMenu.current.removeEventListener('mouseover', closeOnParentMouseOver)
+			}
+		}
+	}, [showMenu])
 
 	return (
-		<span className="dropdown" onBlur={closeMenuOnBlur}>
-			<button
-				ref={toggleMenuButton}
-				type="button"
-				title="Options"
-				className="symbol"
-				onClick={getPositionRelativeToWindow}
-				aria-label="Options"
-				aria-haspopup
-				aria-expanded={showMenu}
-				aria-controls={menuRefId}>{icon}</button>
+		<span
+			ref={menuButton}
+			tabIndex={0}
+			className={submenu ? 'submenu' : 'dropdown'}
+			role="button"
+			title={label}
+			aria-label={label}
+			aria-haspopup
+			aria-expanded={showMenu}
+			aria-controls={menuId}
+			onBlur={closeMenuOnBlur}
+			{...rootMenuOrSubmenuProps}>
+			<span className={`dropdown-button-label${icon ? ' symbol' : ''}`}>
+				{icon || label}
+				{submenu ? <span>chevron_right</span> : <></>}
+			</span>
 			{showMenu ? (
 				<span
+					className="dropdown-content"
 					role="menu"
 					aria-label="Options"
-					id={menuRefId}
-					style={position}>
-					{cloneElement(children, { toggleMenu: toggleShowMenu, navigateWithKeys })}
+					id={menuId}
+					style={position}
+					{...submenu ? { onMouseLeave: stopPropagation } : {}}>
+					{cloneElement(children, { navigateWithKeys, parentMenu: parentMenu || menuButton })}
 				</span>
 			) : <></>}
 		</span>
@@ -68,8 +154,13 @@ const DropdownMenu = ({ icon = 'more_vert', children }) => {
 }
 
 DropdownMenu.propTypes = {
+	children: oneOfType([element, arrayOf(element)]),
 	icon: string,
-	children: oneOfType([element, arrayOf(element)])
+	label: string,
+	parentMenu: shape({
+		current: element
+	}),
+	submenu: bool
 }
 
 export default DropdownMenu
