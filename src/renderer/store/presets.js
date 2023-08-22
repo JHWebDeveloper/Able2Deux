6e24e428e2c83d51f8c2184d893e62d7cf851dbe
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-08-22 17:18:11 -0400

    fist commit

---
+++ src/renderer/store/presets.js
@@ -0,0 +1,51 @@
+import React, { createContext, useEffect } from 'react'
+import toastr from 'toastr'
+import { arrayOf, element, oneOfType } from 'prop-types'
+
+import { presetsReducer as reducer } from 'reducer'
+import { updateState } from 'actions'
+import { useAugmentedDispatch } from 'hooks'
+import { toastrOpts } from 'utilities'
+
+const { interop } = window.ABLE2
+
+const initState = {
+	version: 1
+}
+
+export const PresetsContext = createContext()
+
+export const PresetsProvider = ({ children }) => {
+	const [ state, dispatch ] = useAugmentedDispatch(reducer, initState)
+
+	// useEffect(() => {
+	// 	(async () => {
+	// 		try {
+	// 			dispatch(updateState(await interop.requestPresets()))
+	// 		} catch (err) {
+	// 			toastr.error(err, false, toastrOpts)
+	// 		}
+	// 	})()
+
+	// 	interop.addPrefsSyncListener(newPrefs => {
+	// 		dispatch(updateState(newPrefs))
+	// 	})
+
+	// 	return () => {
+	// 		interop.removePrefsSyncListener()
+	// 	}
+	// }, [])
+
+	return (
+		<PresetsContext.Provider value={{
+			presets: state,
+			dispatch
+		}}>
+			{ children }
+		</PresetsContext.Provider>
+	)
+}
+
+PresetsProvider.propTypes = {
+	children: oneOfType([element, arrayOf(element)]).isRequired
+}
