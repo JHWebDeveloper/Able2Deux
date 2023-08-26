df34113749040581d66f2d89ba20d6d7d242f85c
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-08-26 10:10:37 -0400

    passed prop referencesOnly to function interop.requestPresets args

--- src/renderer/store/presets.js
+++ src/renderer/store/presets.js
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect } from 'react'
 import toastr from 'toastr'
-import { arrayOf, element, oneOfType } from 'prop-types'
+import { arrayOf, bool, element, oneOfType } from 'prop-types'
 
 import { presetsReducer as reducer } from 'reducer'
 import { updateState } from 'actions'
@@ -10,31 +10,33 @@ import { toastrOpts } from 'utilities'
 const { interop } = window.ABLE2
 
 const initState = {
-	version: 1
+	version: 1,
+	presets: [],
+	batchPresets: []
 }
 
 export const PresetsContext = createContext()
 
-export const PresetsProvider = ({ children }) => {
+export const PresetsProvider = ({ referencesOnly, children }) => {
 	const [ state, dispatch ] = useAugmentedDispatch(reducer, initState)
 
-	// useEffect(() => {
-	// 	(async () => {
-	// 		try {
-	// 			dispatch(updateState(await interop.requestPresets()))
-	// 		} catch (err) {
-	// 			toastr.error(err, false, toastrOpts)
-	// 		}
-	// 	})()
+	useEffect(() => {
+		(async () => {
+			try {
+				dispatch(updateState(await interop.requestPresets(referencesOnly)))
+			} catch (err) {
+				toastr.error(err, false, toastrOpts)
+			}
+		})()
 
-	// 	interop.addPrefsSyncListener(newPrefs => {
-	// 		dispatch(updateState(newPrefs))
-	// 	})
+		// interop.addPrefsSyncListener(newPrefs => {
+		// 	dispatch(updateState(newPrefs))
+		// })
 
-	// 	return () => {
-	// 		interop.removePrefsSyncListener()
-	// 	}
-	// }, [])
+		// return () => {
+		// 	interop.removePrefsSyncListener()
+		// }
+	}, [])
 
 	return (
 		<PresetsContext.Provider value={{
@@ -47,5 +49,6 @@ export const PresetsProvider = ({ children }) => {
 }
 
 PresetsProvider.propTypes = {
+	omitPresetAttributes: bool,
 	children: oneOfType([element, arrayOf(element)]).isRequired
 }
