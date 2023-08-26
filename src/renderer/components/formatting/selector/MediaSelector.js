53b04354d25f48cdc68fdcd8a18a5b044e0cb6ed
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-08-26 10:06:17 -0400

    added hide conditions to spacer objects

--- src/renderer/components/formatting/selector/MediaSelector.js
+++ src/renderer/components/formatting/selector/MediaSelector.js
@@ -11,7 +11,7 @@ import {
 	removeAllMedia
 } from 'actions'
 
-import { useWarning } from 'hooks' 
+import { useWarning } from 'hooks'
 
 import MediaInfo from './MediaInfo'
 import BatchList from './BatchList'
@@ -24,7 +24,7 @@ const ctrlOrCmdKeySymbol = interop.isMac ? '⌘' : '⌃'
 const MediaSelector = props => {
 	const { media, focused, multipleItems, multipleItemsSelected, allItemsSelected, dispatch } = props
 	const { presets = [], batchPresets = [] } = useContext(PresetsContext).presets
-
+	
 	const warn = useWarning({ name: 'removeAll' }, [media, allItemsSelected])
 
 	const removeMediaWarning = useCallback(({ message, action }) => warn({
@@ -37,17 +37,18 @@ const MediaSelector = props => {
 	const dropdownDependencies = [media, allItemsSelected, warn, presets, batchPresets]
 
 	const createPresetMenu = useCallback(() => [
-		...presets.map(({ label, hidden }) => ({
+		...presets.map(({ label }) => ({
 			label,
-			hide: hidden,
 			action() {
 				console.log(label)
 			}
 		})),
-		{...presets.length && batchPresets.length ? { type: 'spacer' } : {}},
-		...batchPresets.map(({ label, hidden }) => ({
+		{
+			type: 'spacer',
+			hide: !presets.length || !batchPresets.length
+		},
+		...batchPresets.map(({ label }) => ({
 			label,
-			hide: hidden,
 			action() {
 				console.log(label)
 			}
@@ -80,7 +81,10 @@ const MediaSelector = props => {
 				dispatch(duplicateSelectedMedia())
 			}
 		},
-		{ type: 'spacer' },
+		{
+			type: 'spacer',
+			hide: !multipleItemsSelected
+		},
 		{
 			label: 'Remove Selected',
 			hide: !multipleItemsSelected,
