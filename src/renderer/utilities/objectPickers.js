4df4a3b5786f999cc375af036535f3a34f588fd8
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-03 19:20:29 -0400

    split props lists into sublists where needed. added functions extractRelevantMediaProps, extractCenteringProps, extractAudioProps and extractPositionProps

--- src/renderer/utilities/objectPickers.js
+++ src/renderer/utilities/objectPickers.js
@@ -2,73 +2,67 @@ import { objectPick } from 'utilities'
 
 // ---- LOCAL CONSTANTS --------
 
-const COLOR_CORRECTION_PROPS = ['ccEnabled', 'ccHidden', 'ccRGB', 'ccR', 'ccG', 'ccB']
+const AUDIO_PROPS = Object.freeze(['audioVideoTracks', 'audioExportFormat'])
+const COLOR_CORRECTION_PROPS = Object.freeze(['ccEnabled', 'ccRGB', 'ccR', 'ccG', 'ccB'])
+const CROP_PROPS = Object.freeze(['cropT', 'cropR', 'cropB', 'cropL'])
+const CROP_LINK_PROPS = Object.freeze(['cropLinkTB', 'cropLinkLR'])
+const FRAMING_PROPS = Object.freeze(['arc', 'background', 'bgColor', 'backgroundMotion', 'overlay'])
+const KEYING_PROPS = Object.freeze(['keyingEnabled', 'keyingType', 'keyingColor'])
+const CHROMA_KEY_PROPS = Object.freeze(['keyingSimilarity', 'keyingBlend'])
+const LUMA_KEY_PROPS = Object.freeze(['keyingThreshold', 'keyingTolerance', 'keyingSoftness'])
+const POSITION_PROPS = Object.freeze(['positionX', 'positionY'])
+const ROTATION_BASIC_PROPS = Object.freeze(['transpose', 'reflect'])
+const ROTATION_ADVANCED_PROPS = Object.freeze(['freeRotateMode', 'angle', 'rotatedCentering'])
+const SCALE_PROPS = Object.freeze(['scaleX', 'scaleY'])
+const SOURCE_PROPS = Object.freeze(['sourceName', 'sourcePrefix', 'sourceOnTop'])
 
-const CROP_PROPS = ['cropT', 'cropR', 'cropB', 'cropL']
-
-const CROP_LINK_PROPS = ['cropLinkTB', 'cropLinkLR']
-
-const FRAMING_PROPS = ['arc', 'background', 'bgColor', 'overlay']
-
-const KEYING_PROPS = [
-	'keyingEnabled',
-	'keyingHidden',
-	'keyingType',
-	'keyingColor',
-	'keyingSimilarity',
-	'keyingBlend',
-	'keyingThreshold',
-	'keyingTolerance',
-	'keyingSoftness'
-]
-
-const ROTATION_PROPS = ['transpose', 'reflect', 'freeRotateMode', 'angle', 'rotatedCentering']
-
-const SCALE_PROPS = ['scaleX', 'scaleY']
-
-const SOURCE_PROPS = ['sourceName', 'sourcePrefix', 'sourceOnTop']
-
-/* dyanmicProps are props that can be copied from one media item to another
+/* dynamic props are props that can be copied from one media item to another
     and should cause a preview render on change */
-const DYNAMIC_PROPS = [
+const DYNAMIC_PROPS = Object.freeze([
+	...AUDIO_PROPS,
 	...FRAMING_PROPS,
 	...SCALE_PROPS,
 	...CROP_PROPS,
-	...ROTATION_PROPS,
+	...ROTATION_BASIC_PROPS,
+	...ROTATION_ADVANCED_PROPS,
 	...KEYING_PROPS,
+	...CHROMA_KEY_PROPS,
+	...LUMA_KEY_PROPS,
 	...COLOR_CORRECTION_PROPS,
 	...SOURCE_PROPS,
-	'audioVideoTracks',
-	'audioExportFormat',
-	'centering',
-	'positionX',
-	'positionY'
-]
+	...POSITION_PROPS,
+	'arc',
+	'centering'
+])
 
 // ---- OBJECT PICKERS --------
 
 const createObjectPicker = keys => props => objectPick(props, keys)
 
+export const extractAudioProps = createObjectPicker(AUDIO_PROPS)
+
 export const extractCopyPasteProps = createObjectPicker([
 	...DYNAMIC_PROPS,
 	...CROP_LINK_PROPS,
 	'backgroundMotion',
-	'scaleLink',
-	'ccSelectedCurve'
+	'scaleLink'
 ])
 
 export const extractPreviewRenderDependencies = (() => {
-	const omitNonVisiualProps = createObjectPicker([
+	const getVisualProps = createObjectPicker([
 		...DYNAMIC_PROPS,
+		'keyingHidden',
+		'ccHidden',
 		'id',
 		'timecode'
 	])
 
-	return props => Object.values(omitNonVisiualProps(props))
+	return props => Object.values(getVisualProps(props))
 })()
 
+export const extractCenteringProps = createObjectPicker(['centering'])
 
-export const extractColorCorrectionProps = createObjectPicker([...COLOR_CORRECTION_PROPS, 'ccSelectedCurve'])
+export const extractColorCorrectionProps = createObjectPicker(COLOR_CORRECTION_PROPS)
 
 export const extractCommonPanelProps = createObjectPicker([
 	'id',
@@ -97,12 +91,60 @@ export const extractDefaultPrefs = createObjectPicker([
 	'aspectRatioMarkers'
 ])
 
-export const extractFramingProps = createObjectPicker([...FRAMING_PROPS, 'backgroundMotion'])
+export const extractFramingProps = createObjectPicker(FRAMING_PROPS)
 
-export const extractKeyingProps = createObjectPicker(KEYING_PROPS)
+export const extractKeyingProps = createObjectPicker([...KEYING_PROPS, ...CHROMA_KEY_PROPS, ...LUMA_KEY_PROPS])
 
-export const extractRotationProps = createObjectPicker(ROTATION_PROPS)
+export const extractPositionProps = createObjectPicker(POSITION_PROPS)
+
+export const extractRotationProps = createObjectPicker([...ROTATION_BASIC_PROPS, ...ROTATION_ADVANCED_PROPS])
 
 export const extractScaleProps = createObjectPicker([...SCALE_PROPS, 'scaleLink'])
 
 export const extractSourceProps = createObjectPicker(SOURCE_PROPS)
+
+export const extractRelevantMediaProps = media => {
+	const { mediaType, arc } = media
+	
+	if (mediaType === 'audio') {
+		return objectPick(media, ['audioExportFormat'])
+	} else if (media.audioVideoTracks === 'audio') {
+		return objectPick(media, AUDIO_PROPS)
+	}
+
+	const commonProps = [
+		...FRAMING_PROPS,
+		...COLOR_CORRECTION_PROPS,
+		...ROTATION_BASIC_PROPS
+	]
+
+	if (mediaType === 'video' && media.hasAudio) {
+		commonProps.push(...AUDIO_PROPS)
+	}
+
+	if (arc !== 'none' && media.keyingType === 'luma') {
+		commonProps.push(...KEYING_PROPS, ...LUMA_KEY_PROPS)
+	} else if (arc !== 'none') {
+		commonProps.push(...KEYING_PROPS, ...CHROMA_KEY_PROPS)
+	}
+
+	if (!(arc === 'none' && media.aspectRatio !== '16:9')) {
+		commonProps.push(...SOURCE_PROPS)
+	}
+	
+	if (arc === 'fill') {
+		return objectPick(media, [...commonProps, 'centering'])
+	} else if (arc === 'transform') {
+		return objectPick(media, [
+			...commonProps,
+			...POSITION_PROPS,
+			...SCALE_PROPS,
+			...CROP_PROPS,
+			...CROP_LINK_PROPS,
+			...ROTATION_ADVANCED_PROPS,
+			'scaleLink'
+		])
+  }
+	
+	return objectPick(media, commonProps)
+}
