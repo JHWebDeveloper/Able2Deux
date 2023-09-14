81bfe1b2fad8be3e48248145e1844231385f7727
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-14 02:37:36 -0400

    fixed typo in attribute aria-label

--- src/renderer/components/formatting/SaveButtons.js
+++ src/renderer/components/formatting/SaveButtons.js
@@ -11,7 +11,7 @@ const SaveButtons = ({ setRendering }) => {
 				type="button"
 				className="app-button"
 				title="Back"
-				aria-label="Black"
+				aria-label="Back"
 				onClick={() => navigate('/')}>Back</button>
 			<button
 				type="button"
