be9024b68949a53d6ff1b5f40f5ff586d674f62e
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-16 14:58:16 -0400

    moved replaceTokens and all related functions to module src/renderer/actions/rendering.js. Added function framesToShortTC.

--- src/renderer/utilities/valueModifiers.js
+++ src/renderer/utilities/valueModifiers.js
@@ -30,10 +30,12 @@ const secondsToTCLiterals = sec => [
 	sec % 60
 ].map(lit => lit | 0) // using bitwise or to floor values
 
-// unjoined zeroized values are needed in function getTokenReplacerFns below. not the case for secondsToTC.
+// unjoined zeroized values are needed in function framesToShortTC below. not the case for secondsToTC.
 const framesToTCZeroized = (frms, fps) => framesToTCLiterals(frms, fps).map((n, i) => i === 3 ? zeroizeAuto(n, fps) : zeroize(n))
 
 export const framesToTC = (frms, fps) => framesToTCZeroized(frms, fps).join(':')
+
+export const framesToShortTC = (frms, fps) => arrayInterlace(framesToTCZeroized(frms, fps), TIME_UNIT_S).join('')
 
 export const secondsToTC = sec => secondsToTCLiterals(sec)
 	.map(n => zeroize(n, 2))
@@ -88,58 +90,6 @@ export const format12hr = d => {
 	}
 
 	return `${h}${zeroize(m)}${meridian}`
-}
-
-const getTokenReplacerFns = (i, l, { start, end, duration, fps, instances= [], versions = [], refId, id }) => {
-	const d = new Date()
-
-	return new Map(Object.entries({
-		'$d': () => d.toDateString(),
-		'$D': () => d.toLocaleDateString().replace(/\//g, '-'),
-		'$t': () => format12hr(d),
-		'$T': () => `${d.getHours()}${d.getMinutes()}`,
-		'$i': () => zeroizeAuto(instances.indexOf(refId) + 1, instances.length),
-		'$v': () => zeroizeAuto(versions.indexOf(id) + 1, versions.length),
-		'$n': () => zeroizeAuto(i + 1, l),
-		'$li': () => instances.length,
-		'$lv': () => versions.length,
-		'$l': () => l,
-		'$s': () => arrayInterlace(framesToTCZeroized(start, fps), TIME_UNIT_S).join(''),
-		'$e': () => arrayInterlace(framesToTCZeroized(end, fps), TIME_UNIT_S).join(''),
-		'$r': () => arrayInterlace(framesToTCZeroized(duration * fps, fps), TIME_UNIT_S).join(''),
-		'$c': () => arrayInterlace(framesToTCZeroized(end - start, fps), TIME_UNIT_S).join('')
-	}))
-}
-
-const removeEscapeChars = filename => filename.replace(/\\(?=\$(d|D|t|T|n|i|v|l(i|v)?|s|e|r|c))/g, '')
-
-export const replaceTokens = (filename, i = 0, media) => {
-	if (filename.length < 2) return filename
-
-	const matches = [...new Set(filename.match(/(?<!\\)\$(d|D|t|T|n|i|v|l(i|v)?|s|e|r|c)/g))].sort().reverse()
-
-	if (!media.length) return removeEscapeChars(filename)
-
-	const item = media[i]
-
-	if (matches.includes('$i') || matches.includes('$li')) {
-		item.instances = [...new Set(media.map(({ refId }) => refId))]
-	}
-
-	if (matches.includes('$v') || matches.includes('$lv')) {
-		item.versions = media.reduce((acc, { refId, id }) => {
-			if (refId === item.refId) acc.push(id)
-			return acc
-		}, [])
-	}
-
-	const replacer = getTokenReplacerFns(i, media.length, item)
-	
-	for (const match of matches) {
-		filename = filename.replace(new RegExp(`(?<!\\\\)\\${match}`, 'g'), replacer.get(match)())
-	}
-
-	return removeEscapeChars(filename)
 }
 
 // ---- MISC. --------
