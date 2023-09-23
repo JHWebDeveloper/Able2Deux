41f07be41fae8f519e0daba3e320bc45a12e8d28
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-23 17:34:48 -0400

    wrapped entire RadioSet render loop in component FieldsetWrapper.js

--- src/renderer/components/form_elements/RadioSet.js
+++ src/renderer/components/form_elements/RadioSet.js
@@ -1,22 +1,31 @@
 import React, { useId } from 'react'
 import { arrayOf, element, func, shape, string } from 'prop-types'
 
-const RadioSet = ({ name, state, onChange, buttons = [] }) => {
+import FieldsetWrapper from './FieldsetWrapper'
+
+const RadioSet = ({ label, hideLabel, disabled, name, state, onChange, buttons = [] }) => {
 	const setKey = useId()
 
-	return buttons.map(({ label, value, component }, i) => (
-		<label key={`${setKey}_${i}`}>
-			<input
-				type="radio"
-				name={name}
-				title={label}
-				aria-label={label}
-				value={value}
-				checked={state === value}
-				onChange={onChange} />
-			{component || <span>{label}</span>}
-		</label>
-	))
+	return (
+    <FieldsetWrapper
+      label={label}
+      hideLabel={hideLabel}
+      disabled={disabled}>
+			{buttons.map(({ label, value, component }, i) => (
+				<label key={`${setKey}_${i}`}>
+					<input
+						type="radio"
+						name={name}
+						title={label}
+						aria-label={label}
+						value={value}
+						checked={state === value}
+						onChange={onChange} />
+					{component || <span>{label}</span>}
+				</label>
+			))}
+		</FieldsetWrapper>
+	)
 }
 
 RadioSet.propTypes = {
