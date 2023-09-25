0ccb07dfa1a1796340986f7de4b133925da2647f
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-24 23:56:52 -0400

    adde props className and hideLabel

--- src/renderer/components/form_elements/FieldsetWrapper.js
+++ src/renderer/components/form_elements/FieldsetWrapper.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { arrayOf, bool, element, oneOfType, string } from 'prop-types'
 
-const FieldsetWrapper = ({ label, hideLabel, disabled, children }) => {
-	return (
-		<fieldset
-			className={`radio-set${hideLabel ? ' hide-label' : ''}`}
-			disabled={disabled}>
-			<legend>{label}<span aria-hidden>:</span></legend>
-			{children}
-		</fieldset>
-	)
-}
+const FieldsetWrapper = ({ label, className, hideLabel, disabled, children }) => (
+	<fieldset
+		className={`${className}${hideLabel ? `${className ? ' ' : ''}hide-label` : ''}`}
+		disabled={disabled}>
+		<legend>{label}<span aria-hidden>:</span></legend>
+		{children}
+	</fieldset>
+)
 
 FieldsetWrapper.propTypes = {
 	label: string.isRequired,
+	className: string,
 	hideLabel: bool,
 	disabled: bool,
 	children: oneOfType([element, arrayOf(element)]).isRequired
