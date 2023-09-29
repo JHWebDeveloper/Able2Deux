4e789a3a8afc923def2cf777d1cfa3eb2ba8e4de
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-28 21:52:05 -0400

    added function loadPresetsForEditing

--- src/renderer/reducer/presets.js
+++ src/renderer/reducer/presets.js
@@ -9,7 +9,20 @@ export const presetsReducer = (state, action) => {
 	switch (type) {
 		case ACTION.UPDATE_STATE:
 			return shared.updateState(state, payload)
+		case ACTION.LOAD_PRESETS_FOR_EDITING:
+			return loadPresetsForEditing(state, payload)
 		default:
 			return state
+	}
+}
+
+const loadPresetsForEditing = (state, payload) => {
+	return {
+		...state,
+		...payload,
+		presets: payload.presets.map((item, i) => ({
+			...item,
+			focused: i === 0
+		}))
 	}
 }
