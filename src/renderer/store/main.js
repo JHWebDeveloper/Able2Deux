2a596718b93a01365754a1d68a9bc31561e506dd
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-09-28 21:49:45 -0400

    removed PrefsProvider. Now handling merging of preferences state in App.js

--- src/renderer/store/main.js
+++ src/renderer/store/main.js
@@ -1,12 +1,9 @@
-import React, { createContext, useContext, useEffect } from 'react'
+import React, { createContext } from 'react'
 import { arrayOf, element, oneOfType } from 'prop-types'
 
-import { PrefsProvider, PrefsContext } from 'store'
-
 import { mainReducer as reducer } from 'reducer'
-import { updateState } from 'actions'
 import { useAugmentedDispatch } from 'hooks'
-import { createObjectPicker, pipe } from 'utilities'
+
 
 const initState = {
 	url: '',
@@ -25,49 +22,21 @@ const initState = {
 	rendering: false
 }
 
-const extractDefaultPrefs = createObjectPicker([
-	'saveLocations',
-	'split',
-	'optimize',
-	'timerEnabled',
-	'timer',
-	'screenshot',
-	'previewQuality',
-	'previewHeight',
-	'aspectRatioMarkers'
-])
-
 export const MainContext = createContext()
 
-const MainProviderWithDefaultPrefs = ({ children }) => {
+export const MainProvider = ({ children }) => {
 	const [ state, dispatch ] = useAugmentedDispatch(reducer, initState)
-	const { preferences } = useContext(PrefsContext)
-
-	useEffect(() => {
-		pipe(extractDefaultPrefs, updateState, dispatch)(preferences)
-	}, [preferences])
 
 	return (
 		<MainContext.Provider value={{
 			...state,
 			dispatch
 		}}>
-			{ children }
+			{children}
 		</MainContext.Provider>
 	)
 }
 
-export const MainProvider = ({ children }) => (
-	<PrefsProvider>
-		<MainProviderWithDefaultPrefs>
-			{ children }
-		</MainProviderWithDefaultPrefs>
-	</PrefsProvider>
-)
-
-const providerPropTypes = {
+MainProvider.propTypes = {
 	children: oneOfType([element, arrayOf(element)]).isRequired
 }
-
-MainProviderWithDefaultPrefs.propTypes = providerPropTypes
-MainProvider.propTypes = providerPropTypes
