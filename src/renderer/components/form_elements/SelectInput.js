18a3fee9c92f2b11cac1fd3c93208e5093fe4a90
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-10-11 21:10:57 -0400

    added key to option element and created variable setKey with useId hook

--- src/renderer/components/form_elements/SelectInput.js
+++ src/renderer/components/form_elements/SelectInput.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useId } from 'react'
 import { arrayOf, bool, exact, func, string } from 'prop-types'
 
-const SelectInput = ({ name, value, onChange, disabled, options, title, ariaLabelledby }) => (
-  <select
-    className="panel-input"
-    name={name}
-    value={value}
-    onChange={onChange}
-    disabled={disabled}
-    {...title ? { title } : {}}
-    {...ariaLabelledby ? { 'aria-labelledby': ariaLabelledby } : title ? { 'aria-label': title } : {}}>
-    {options.map(opt => <option value={opt.value}>{opt.label}</option>)}
-  </select>
-)
+const SelectInput = ({ name, value, onChange, disabled, options, title, ariaLabelledby }) => {
+  const setKey = useId()
+
+  return (
+    <select
+      className="panel-input"
+      name={name}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      {...title ? { title } : {}}
+      {...ariaLabelledby ? { 'aria-labelledby': ariaLabelledby } : title ? { 'aria-label': title } : {}}>
+      {options.map((opt, i) => (
+        <option
+          key={`${setKey}_${i}`}
+          value={opt.value}>
+          {opt.label}
+        </option>
+      ))}
+    </select>
+  )
+}
 
 SelectInput.propTypes = {
   name: string.isRequired,
