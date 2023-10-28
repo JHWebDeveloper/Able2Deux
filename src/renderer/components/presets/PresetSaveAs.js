9a76000c8d06ac8d72d1f7f0d2e76abd0f9fd13a
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-10-28 19:05:32 -0400

    replaced local state with new PresetsSaveAsProvider so preset save as can use the same actions as presets

--- src/renderer/components/presets/PresetSaveAs.js
+++ src/renderer/components/presets/PresetSaveAs.js
@@ -3,19 +3,21 @@ import { HashRouter, NavLink, Routes, Route } from 'react-router-dom'
 import toastr from 'toastr'
 import '../../css/preset_save_as.css'
 
-import { PresetsProvider, PresetsContext } from 'store'
-
-import { MEDIA_TYPES, TOASTR_OPTIONS } from 'constants'
-
 import {
-	errorToString,
-	createAttributesFromPresetAttributeProperty as createNewAttributesFromMediaState,
-	pipe
-} from 'utilities'
+	PresetsProvider,
+	PresetsContext,
+	PresetsSaveAsProvider,
+	PresetsSaveAsContext
+} from 'store'
+
+import { togglePresetLimitTo, updatePresetStateBySelection } from 'actions'
+import { TOASTR_OPTIONS } from 'constants'
+import { errorToString, createPresetFromAttributeSet } from 'utilities'
 
 import MainForm from '../form_elements/MainForm'
 import RadioSet from '../form_elements/RadioSet'
 import FieldsetWrapper from '../form_elements/FieldsetWrapper'
+import SelectInput from '../form_elements/SelectInput'
 import ButtonWithIcon from '../form_elements/ButtonWithIcon'
 import AttributeSelector from './AttributeSelector'
 import PresetOptions from './PresetOptions'
@@ -37,106 +39,53 @@ const SAVE_TYPE_BUTTONS = Object.freeze([
 	}
 ])
 
-const removeOutterAttributes = attributes => attributes.filter(attr => attr.include)
-
-const includeAttributesByMediaState = mediaState => attributes => attributes.map(attr => {
-	switch (attr.attribute) {
-		case 'overlay':
-		case 'background':
-			attr.include = mediaState.arc !== 'none'
-			break
-		case 'bgColor':
-			attr.include = mediaState.arc !== 'none' && mediaState.background === 'color'
-			break
-		case 'backgroundMotion':
-			attr.include = mediaState.arc !== 'none' && mediaState.background !== 'alpha' && mediaState.background !== 'color'
-			break
-		case 'sourceName':
-		case 'sourcePrefix':
-		case 'sourceOnTop':
-			attr.include = !!mediaState.sourceName
-			break
-		case 'rotatedCentering':
-			attr.include = mediaState.freeRotateMode === 'inside_bounds'
-			break
-		case 'keyingType':
-		case 'keyingColor':
-		case 'keyingSimilarity':
-		case 'keyingBlend':
-		case 'keyingThreshold':
-		case 'keyingTolerance':
-		case 'keyingSoftness':
-			attr.include = mediaState.keyingEnabled
-			break
-		case 'ccRGB':
-		case 'ccR':
-		case 'ccG':
-		case 'ccB':
-			attr.include = mediaState.ccEnabled
-			break
-		default:
-			attr.include = true
-	}
-
-	return attr
-})
-
-const mapPresetToAttributes = preset => preset.reduce((acc, { include, attribute, value }) => {
-	if (include) acc[attribute] = value
-	return acc
-}, {})
-
 const PresetSaveAs = () => {
-	const { presets: existingPresets } = useContext(PresetsContext).presets
+	const { presets: existingPresets = [] } = useContext(PresetsContext).presets
+	const { presets: { presets: [ newPreset = {} ] }, dispatch } = useContext(PresetsSaveAsContext)
+	const [ saveType, setSaveType ] = useState('newPreset')
+	const [ selectedPreset, setSelectedPreset ] = useState('')
 
-	const [ state, updateState ] = useState({
-		saveType: 'newPreset',
-		presetName: '',
-		selectedPreset: '',
-		attributes: [],
-		presetNamePrepend: '',
-		presetNameAppend: '',
-		limitTo: [...MEDIA_TYPES]
-	})
+	const {
+		label = '',
+		attributes = [],
+		presetNamePrepend = '',
+		presetNameAppend = '',
+		limitTo = []
+	} = newPreset
 
-	const { saveType, presetName, selectedPreset, attributes, presetNamePrepend, presetNameAppend, limitTo } = state
-	const saveEnabled = (saveType === 'newPreset' && presetName.length || selectedPreset) && attributes.some(({ include }) => include) && limitTo.length
+	const saveEnabled = ((saveType === 'newPreset' && label.length) || (saveType !== 'newPreset' && selectedPreset)) && attributes.some(({ include }) => include) && limitTo.length
 
-	const updateStateFromEvent = useCallback(e => {
-		updateState(currentState => ({
-			...currentState,
+	const updatePresetStateFromEvent = useCallback(e => {
+		dispatch(updatePresetStateBySelection({
 			[e.target.name]: e.target.value
 		}))
 	}, [])
 
-	const toggleLimitTo = useCallback(e => {
-		const { name, checked } = e?.target || e
+	const togglePresetLimitToFromEvent = useCallback(e => {
+		dispatch(togglePresetLimitTo(e.target.name))
+	}, [])
 
-		updateState(currentState => ({
-			...currentState,
-			limitTo: !checked
-				? currentState.limitTo.filter(mediaType => mediaType !== name)
-				: [...currentState.limitTo, name].toSorted()
-		}))
+	const setSaveTypeFromEvent = useCallback(e => {
+		setSaveType(e.target.value)
+	}, [])
+
+	const setSelectedPresetFromEvent = useCallback(e => {
+		setSelectedPreset(e.target.value)
 	}, [])
 
 	const savePreset = async () => {
 		try {
-			await interop.savePreset({
-				type: 'preset',
+			await interop.savePreset(createPresetFromAttributeSet({
 				limitTo,
-				attributes: {
-					...mapPresetToAttributes(attributes),
-					...presetNamePrepend ? { presetNamePrepend } : {},
-					...presetNameAppend ? { presetNameAppend } : {}
-				},
+				presetNamePrepend,
+				presetNameAppend,
+				attributes,
 				...saveType === 'newPreset' ? {
-					label: presetName
+					label
 				} : {
-					id: selectedPreset,
-					overwrite: saveType === 'replace'
+					id: selectedPreset
 				}
-			})
+			}), saveType)
 	
 			interop.closePresetSaveAs()
 		} catch (err) {
@@ -145,27 +94,7 @@ const PresetSaveAs = () => {
 	}
 
 	useEffect(() => {
-		(async () => {
-			let { presetNamePrepend, presetNameAppend, ...mediaState } = await interop.getPresetToSave()
-
-			updateState(currentState => ({
-				...currentState,
-				attributes: pipe(
-					createNewAttributesFromMediaState,
-					removeOutterAttributes,
-					includeAttributesByMediaState(mediaState)
-				)(mediaState),
-				presetNamePrepend: presetNamePrepend ?? '',
-				presetNameAppend: presetNameAppend ?? ''
-			}))
-		})()
-	}, [])
-
-	useEffect(() => {
-		if (saveType !== 'newPreset' && !selectedPreset) updateState(currentState => ({
-			...currentState,
-			selectedPreset: existingPresets[0].id
-		}))
+		if (saveType !== 'newPreset' && !selectedPreset) setSelectedPreset(existingPresets[0].id)
 	}, [saveType, existingPresets])
 
 	return (
@@ -180,7 +109,7 @@ const PresetSaveAs = () => {
 							label="Save Type"
 							name="saveType"
 							state={saveType}
-							onChange={updateStateFromEvent}
+							onChange={setSaveTypeFromEvent}
 							buttons={SAVE_TYPE_BUTTONS}/>
 					) : <></>}
 					{saveType === 'newPreset' ? (
@@ -188,22 +117,18 @@ const PresetSaveAs = () => {
 							<input
 								type="text"
 								className="panel-input"
-								name="presetName"
+								name="label"
 								maxLength={50}
-								value={presetName}
-								onChange={updateStateFromEvent} />
+								value={label}
+								onChange={updatePresetStateFromEvent} />
 						</FieldsetWrapper>
 					) : (
 						<FieldsetWrapper label="Select Preset">
-							<select
-								className="panel-input"
+							<SelectInput
 								name="selectedPreset"
 								value={selectedPreset}
-								onChange={updateStateFromEvent}>
-								{existingPresets.map(({ id, label }) => (
-									<option key={id} value={id}>{label}</option>
-								))}
-							</select>
+								onChange={setSelectedPresetFromEvent}
+								options={existingPresets} />
 						</FieldsetWrapper>
 					)}
 				</section>
@@ -219,15 +144,15 @@ const PresetSaveAs = () => {
 									<Route path="/" element={
 										<AttributeSelector
 											attributes={attributes}
-											updateState={updateState} />
+											dispatch={dispatch} />
 									} />
 									<Route path="/options" element={
 										<PresetOptions
 											presetNamePrepend={presetNamePrepend}
 											presetNameAppend={presetNameAppend}
 											limitTo={limitTo}
-											updatePresetName={updateStateFromEvent}
-											toggleLimitTo={toggleLimitTo} />
+											updatePresetState={updatePresetStateFromEvent}
+											toggleLimitTo={togglePresetLimitToFromEvent} />
 									} />
 								</Routes>
 							</div>
@@ -254,6 +179,8 @@ const PresetSaveAs = () => {
 
 export default () => (
 	<PresetsProvider referencesOnly presorted>
-		<PresetSaveAs />
+		<PresetsSaveAsProvider>
+			<PresetSaveAs />
+		</PresetsSaveAsProvider>
 	</PresetsProvider>
 )
