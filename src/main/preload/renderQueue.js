2333e02027b9b5f3cd58170bfb52ddc09bf26d71
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-09 16:17:22 -0500

    first commit

---
+++ src/main/preload/renderQueue.js
@@ -0,0 +1,41 @@
+import { ipcRenderer } from 'electron'
+
+import { sendMessage, requestChannel } from './sendMessage'
+
+export const openRenderQueue = data => sendMessage({
+  sendMsg: 'openRenderQueue',
+  recieveMsg: 'renderQueueOpened',
+  errMsg: 'openRenderQueue',
+  data
+})
+
+export const getMediaToRender= () => ipcRenderer.invoke('getMediaToRender')
+
+export const requestRenderChannel = params => {
+	const { data, startCallback, progressCallback } = params
+	
+	return requestChannel({
+		sendMsg: 'requestRender',
+		recieveMsg: `renderComplete_${data.id}`,
+		errMsg: `renderFailed_${data.id}`,
+		data,
+		startMsg: `renderStarted_${data.id}`,
+		progressMsg: `renderProgress_${data.id}`,
+		startCallback,
+		progressCallback
+	})
+}
+
+export const cancelRender = id => ipcRenderer.send('cancelRender', id)
+
+export const closeRenderQueue = startOver => ipcRenderer.send('closeRenderQueue', startOver)
+
+export const setStartOverListener = callback => {
+	ipcRenderer.on('startOver', () => {
+		callback()
+	})
+}
+
+export const removeStartOverListener  = () => {
+	ipcRenderer.removeAllListeners('startOver')
+}
