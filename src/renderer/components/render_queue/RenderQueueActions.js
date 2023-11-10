db260c0a3092d8324ae7d1e5c57581d56ce16b4c
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-10 10:51:31 -0500

    first commit

---
+++ src/renderer/components/render_queue/RenderQueueActions.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { arrayOf, func, oneOf, shape } from 'prop-types'
+
+import { STATUS } from 'constants'
+
+import ButtonWithIcon from '../form_elements/ButtonWithIcon'
+
+const { interop } = window.ABLE2
+
+const RenderQueueActions = ({ media, cancelRender, dispatch }) => {
+  const cancelAllRenders = () => {
+    for (const { id, renderStatus } of media) {
+      dispatch(cancelRender(id, renderStatus))
+    }
+  }
+
+	const complete = media.every(({ renderStatus }) => (
+    renderStatus === STATUS.COMPLETE ||
+    renderStatus === STATUS.CANCELLED ||
+    renderStatus === STATUS.FAILED
+  ))
+
+  return (
+    <div className="action-buttons">
+      {complete ? (
+        <>
+          <ButtonWithIcon
+            label="Close"
+            icon="close"
+            onClick={() => interop.closeRenderQueue()}
+            autoFocus />
+          <ButtonWithIcon
+            label="Start Over"
+            icon="restart_alt"
+            onClick={() => interop.closeRenderQueue(true)} />
+        </>
+      ) : (
+        <ButtonWithIcon
+          label="Cancel All"
+          icon="close"
+          onClick={cancelAllRenders}
+          autoFocus />
+      )}
+    </div>
+  )
+}
+
+RenderQueueActions.propTypes = {
+  media: arrayOf(shape({
+    renderStatus: oneOf([STATUS.PENDING, STATUS.RENDERING, STATUS.COMPLETE, STATUS.FAILED, STATUS.CANCELLED])
+  })),
+  cancelRender: func.isRequired,
+  dispatch: func.isRequired
+}
+
+export default RenderQueueActions
