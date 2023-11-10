5fb8549555065064eb19166c86270d553299c84d
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-10 10:54:25 -0500

    changed Save button to now open and transfer state to render queue window

--- src/renderer/components/formatting/SaveButtons.js
+++ src/renderer/components/formatting/SaveButtons.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
-import { func } from 'prop-types'
+import { arrayOf, bool, exact, object, oneOf, string } from 'prop-types'
 
 import ButtonWithIcon from '../form_elements/ButtonWithIcon'
 
-const SaveButtons = ({ setRendering }) => {
+const { interop } = window.ABLE2
+
+const SaveButtons = ({ media, batchName, saveLocations }) => {
 	const navigate = useNavigate()
 	
 	return (
@@ -16,13 +18,26 @@ const SaveButtons = ({ setRendering }) => {
 			<ButtonWithIcon
 				label="Save"
 				icon="save"
-				onClick={() => setRendering(true)} />
+				onClick={() => interop.openRenderQueue({ media, batchName, saveLocations })} />
 		</div>
 	)
 }
 
 SaveButtons.propTypes = {
-	setRendering: func.isRequired
+	media: arrayOf(object).isRequired,
+	batchName: exact({
+		batchNameType: oneOf(['replace', 'prepend_append']),
+		batchName: string,
+		batchNamePrepend: string,
+		batchNameAppend: string,
+	}).isRequired,
+	saveLocations: arrayOf(exact({
+		checked: bool,
+		directory: string,
+		hidden: bool,
+		id: string,
+		label: string
+	})).isRequired
 }
 
 export default SaveButtons
