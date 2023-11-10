3ffac1a29055975fc285857c7ac8c9f8cb53bd8b
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-09 20:30:21 -0500

    added function extractDefaultPrefs. merging default prefs with state once prefsLoaded is true.

--- src/renderer/store/main.js
+++ src/renderer/store/main.js
@@ -1,9 +1,11 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext, useEffect, useRef } from 'react'
 import { arrayOf, element, oneOfType } from 'prop-types'
 
 import { mainReducer as reducer } from 'reducer'
+import { PrefsContext } from 'store'
+import { updateState } from 'actions'
 import { useAugmentedDispatch } from 'hooks'
-
+import { createObjectPicker, pipe } from 'utilities'
 
 const initState = {
 	url: '',
@@ -19,13 +21,32 @@ const initState = {
 	batchNameAppend: '',
 	previewQuality: 1,
 	previewHeight: 540,
-	rendering: false
+	aspectRatioMarkers: [],
+	saveLocations: []
 }
+
+const extractDefaultPrefs = createObjectPicker([
+	'saveLocations',
+	'split',
+	'optimize',
+	'timerEnabled',
+	'timer',
+	'screenshot',
+	'previewQuality',
+	'previewHeight',
+	'aspectRatioMarkers'
+])
 
 export const MainContext = createContext()
 
 export const MainProvider = ({ children }) => {
 	const [ state, dispatch ] = useAugmentedDispatch(reducer, initState)
+	const { preferences, prefsLoaded } = useContext(PrefsContext)
+	const { aspectRatioMarkers, saveLocations } = preferences
+
+	useEffect(() => {
+		if (prefsLoaded) pipe(extractDefaultPrefs, updateState, dispatch)(preferences)
+	}, [prefsLoaded, aspectRatioMarkers, saveLocations])
 
 	return (
 		<MainContext.Provider value={{
