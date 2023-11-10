f4a8f9110171f610dc743453f88250b2f46d6629
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-10 10:51:18 -0500

    effectively recoded entire module as previous commit was just a progress save

--- src/renderer/components/render_queue/RenderQueue.js
+++ src/renderer/components/render_queue/RenderQueue.js
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { useContext, useMemo, useRef } from 'react'
 
-import { RenderQueueProvider } from 'store'
+import { PrefsContext, PrefsProvider, RenderQueueContext, RenderQueueProvider } from 'store'
+import { createCancelRenderAction, createRenderAction } from 'actions'
+import { createPromiseQueue } from 'utilities'
+
+import RenderQueueItem from './RenderQueueItem'
+import RenderQueueActions from './RenderQueueActions'
 
 const RenderQueue = () => {
+  const { autoPNG, concurrent, customFrameRate, renderFrameRate, renderOutput } = useContext(PrefsContext).preferences
+  const { media, directories, dispatch } = useContext(RenderQueueContext)
+  const promiseQueue = useRef(createPromiseQueue(concurrent))
+
+  const renderMedia = useMemo(() => createRenderAction({
+    autoPNG,
+    customFrameRate,
+    directories,
+    renderFrameRate,
+    renderOutput
+  }), [])
+
+  const cancelRender = useMemo(() => createCancelRenderAction(promiseQueue.current), [])
+
   return (
-    <RenderQueueProvider>
-      <div>RenderQueue</div>
-    </RenderQueueProvider>
+    <main className="queue-list">
+      <div className="populated">
+        {media.map((item, i) => (
+          <RenderQueueItem
+            key={item.id}
+            item={item}
+            promiseQueue={promiseQueue.current}
+            isLast={i === media.length - 1}
+            renderMedia={renderMedia}
+            cancelRender={cancelRender}
+            dispatch={dispatch} />
+        ))}
+      </div>
+      <RenderQueueActions
+        media={media}
+        cancelRender={cancelRender}
+        dispatch={dispatch} />
+    </main>
   )
 }
 
-export default RenderQueue
+const RenderQueueMountWithAsyncDependencies = () => {
+  const { prefsLoaded } = useContext(PrefsContext)
+
+  return prefsLoaded
+    ? <RenderQueue />
+    : <></>
+}
+
+export default () => (
+  <PrefsProvider>
+    <RenderQueueProvider>
+      <RenderQueueMountWithAsyncDependencies />
+    </RenderQueueProvider>
+  </PrefsProvider>
+)
