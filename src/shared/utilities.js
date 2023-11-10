c7175c9533199be7ef59e0bb9a05df970c2bfdfb
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-09 20:37:01 -0500

    added function clamp

--- src/shared/utilities.js
+++ src/shared/utilities.js
@@ -1,3 +1,7 @@
+// ---- CALCULATORS --------
+
+const clamp = (val, min, max) => Math.max(min, Math.min(max, val))
+
 const degToRad = deg => deg * Math.PI / 180
 
 const calcRotatedDimension = (w, h, trig1, trig2) => w * trig1 + h * trig2
@@ -19,6 +23,8 @@ const calcRotatedBoundingBox = (w, h, rad, dimension) => {
 	}
 }
 
+// ---- NUMBER METHOD EXTENSIONS --------
+
 const getIntegerLength = n => {
 	if (n < 0) n *= -1
 
@@ -32,12 +38,15 @@ const getIntegerLength = n => {
 	return count
 }
 
+// ---- OBJECT METHOD EXTENSIONS --------
+
 const objectPick = (obj, props) => props.reduce((extract, prop) => {
 	if (prop in obj) extract[prop] = obj[prop]
 	return extract
 }, {})
 
 module.exports = {
+	clamp,
 	calcRotatedBoundingBox,
 	degToRad,
 	getIntegerLength,
