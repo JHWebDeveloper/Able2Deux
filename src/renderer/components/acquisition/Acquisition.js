e2edb889492bc0c0a5a99ffe12ba98c72a1b5bb2
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-12 15:01:26 -0500

    imported pendingMedia and importQueueDispatch from ImportQueueContext. passed importQueueDispatch to components Downloader, Uploader and ReadyQueue. passed pendingMedia to component ReadyQueue. added useEffect containing loop that automatically transers all ImportQueueContext media elements to MainContext when status is READY on remount.

--- src/renderer/components/acquisition/Acquisition.js
+++ src/renderer/components/acquisition/Acquisition.js
@@ -1,7 +1,9 @@
-import React, { useContext, useMemo, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import 'css/index/acquisition.css'
 
-import { MainContext, PrefsContext } from 'store'
+import { ImportQueueContext, ImportQueueProvider, MainContext, PrefsContext } from 'store'
+import { addMedia, removeSortableElement } from 'actions'
+import { STATUS } from 'constants'
 
 import Downloader from './Downloader'
 import Uploader from './Uploader'
@@ -10,10 +12,20 @@ import ReadyQueue from './ReadyQueue'
 
 const Acquisition = () => {
 	const [ recording, setRecording ] = useState(false)
+	const { media: pendingMedia, dispatch: importQueueDispatch } = useContext(ImportQueueContext)
 	const { url, optimize, screenshot, timer, timerEnabled, media, dispatch } = useContext(MainContext)
 	const { disableRateLimit, renderOutput, screenRecorderFrameRate, warnings } = useContext(PrefsContext).preferences
 
 	const output = useMemo(() => renderOutput.split('x')[1], [renderOutput])
+
+	useEffect(() => {
+		for (const item of pendingMedia) {
+			if (item.status === STATUS.READY) {
+				importQueueDispatch(removeSortableElement(item.id))
+				dispatch(addMedia(item))
+			}
+		}
+	}, [pendingMedia])
 
 	return (
 		<>
@@ -22,8 +34,9 @@ const Acquisition = () => {
 				optimize={optimize}
 				output={output}
 				disableRateLimit={disableRateLimit}
-				dispatch={dispatch} />
-			<Uploader dispatch={dispatch} />
+				dispatch={dispatch}
+				importQueueDispatch={importQueueDispatch} />
+			<Uploader importQueueDispatch={importQueueDispatch} />
 			<ScreenRecorder
 				recording={recording}
 				setRecording={setRecording}
@@ -33,12 +46,18 @@ const Acquisition = () => {
 				timerEnabled={timerEnabled}
 				dispatch={dispatch} />
 			<ReadyQueue
+				pendingMedia={pendingMedia}
 				media={media}
 				recording={recording}
 				warnings={warnings}
-				dispatch={dispatch} />
+				dispatch={dispatch}
+				importQueueDispatch={importQueueDispatch} />
 		</>
 	)
 }
 
-export default Acquisition
+export default () => (
+	<ImportQueueProvider>
+		<Acquisition />
+	</ImportQueueProvider>
+)
