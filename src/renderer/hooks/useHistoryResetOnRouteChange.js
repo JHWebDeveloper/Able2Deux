24da1e5cd7c5dd31b63bcd561527b7a9ba77220b
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-14 10:32:58 -0500

    first commit

---
+++ src/renderer/hooks/useHistoryResetOnRouteChange.js
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react'
+import { useLocation } from 'react-router-dom'
+
+import { clearUndoHistory } from 'actions'
+
+export const useHistoryResetOnRouteChange = dispatch => {
+  const firstPageLoad = useRef(true)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (firstPageLoad.current) {
+      firstPageLoad.current = false
+    } else {
+      dispatch(clearUndoHistory())
+    }
+  }, [location])
+}
