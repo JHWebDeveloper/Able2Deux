35ff0cae3a641345ec5c95ae16f32e6639e2faf2
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-15 20:33:49 -0500

    removed Default Preview Quality setting

--- src/renderer/components/preferences/PreviewSettings.js
+++ src/renderer/components/preferences/PreviewSettings.js
@@ -1,44 +1,16 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useContext } from 'react'
 
 import { PrefsContext } from 'store'
 
-import { updateState, updateStateFromEvent } from 'actions'
+import { updateStateFromEvent } from 'actions'
 
-import RadioSet from '../form_elements/RadioSet'
 import AspectRatioMarkers from './AspectRatioMarkers'
-
-const QUALITY_OPTIONS = Object.freeze([
-	{
-		label: '100%',
-		value: '1'
-	},
-	{
-		label: '75%',
-		value: '0.75'
-	},
-	{
-		label: '50%',
-		value: '0.5'
-	}
-])
 
 const PreviewSettings = () => {
 	const { preferences, dispatch } = useContext(PrefsContext)
 
-	const setPreviewQuality = useCallback(e => {
-		dispatch(updateState({
-			previewQuality: parseFloat(e.target.value)
-		}))
-	}, [])
-
 	return (
 		<>
-			<RadioSet
-				label="Default Quality"
-				name="previewQuality"
-				options={QUALITY_OPTIONS}
-				state={preferences.previewQuality.toString()}
-				onChange={setPreviewQuality} />
 			<label className="label-with-input">
 				<span>Grid Color</span>
 				<input
