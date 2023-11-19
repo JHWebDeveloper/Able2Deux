89402faa9fc7a98dc9991d7cf1e2ce6c61c2aa3f
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-19 02:13:29 -0500

    first commit

---
+++ src/renderer/components/formatting/selector/Thumbnail.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { oneOf, string } from 'prop-types'
+
+import { MEDIA_TYPES } from 'constants'
+
+const { interop } = window.ABLE2
+
+const Thumbnail = ({ mediaType, refId, title }) => {
+	const [ thumbnail, setThumbnail ] = useState(null)
+
+	useEffect(() => {
+		(async () => {
+			setThumbnail(await interop.requestThumbnail(mediaType !== 'audio' && refId))
+		})()
+	}, [refId])
+
+	return (
+		<img
+			src={thumbnail}
+			alt={title}
+			draggable="false" />
+	)
+}
+
+Thumbnail.propTypes = {
+	mediaType: oneOf(MEDIA_TYPES),
+	refId: string,
+	title: string
+}
+
+export default Thumbnail
