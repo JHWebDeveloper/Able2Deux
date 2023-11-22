d5c51c04bd6fc7bdefd670d02b3a900d0970edf8
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-22 16:56:54 -0500

    first commit

---
+++ src/shared/constants.js
@@ -0,0 +1,111 @@
+const INIT_PREFS_STATE = Object.freeze({
+	theme: 'system',
+	scratchDisk: {
+		imports: '',
+		exports: '',
+		previews: ''
+	},
+	warnings: {
+		remove: true,
+		removeReferenced: true,
+		removeAll: true,
+		applyToAll: true,
+		sourceOnTop: true,
+		startOver: true,
+		removePreset: true,
+		removeReferencedPreset: true,
+		removePresetFromBatch: true
+	},
+	optimize: 'quality',
+	screenshot: false,
+	timerEnabled: false,
+	screenRecorderFrameRate: 60,
+	timer: 60,
+	editorSettings: {
+		arc: 'none',
+		backgroundMotion: 'animated'
+	},
+	selectAllByDefault: false,
+	enable11pmBackgrounds: false,
+	sliderSnapPoints: true,
+	split: 270,
+	scaleSliderMax: 400,
+	gridColor: '#ff00ff',
+	aspectRatioMarkers: [],
+	renderOutput: '1280x720',
+	renderFrameRate: 'auto',
+	customFrameRate: 23.98,
+	autoPNG: true,
+	batchNameSeparator: ' ',
+	replaceSpaces: false,
+	spaceReplacement: '_',
+	convertCase: false,
+	casing: 'lowercase',
+	dateTimeSource: 'renderStarted',
+	asperaSafe: true,
+	concurrent: 2,
+	saveLocations: [],
+	disableRateLimit: false
+})
+
+const INIT_PRESETS_STATE = Object.freeze({
+  presets: []
+})
+
+const INIT_WORKSPACE_STATE = Object.freeze({
+	windowWidth: 830,
+	windowHeight: 800,
+	previewHeight: 540,
+	previewQuality: 1,
+	grid: false,
+	panels: {
+		batchName: {
+			open: false
+		},
+		preview: {
+			open: true
+		},
+		fileOptions: {
+			open: true
+		},
+		audio: {
+			open: false
+		},
+		framing: {
+			open: true
+		},
+		source: {
+			open: true
+		},
+		centering: {
+			open: true
+		},
+		position: {
+			open: false
+		},
+		scale: {
+			open: false
+		},
+		crop: {
+			open: false
+		},
+		rotation: {
+			open: false
+		},
+		keying: {
+			open: false
+		},
+		colorCorrection: {
+			open: false
+		},
+		presetNameTemplate: {
+			open: false
+		}
+	}
+})
+
+module.exports = {
+  INIT_PREFS_STATE,
+  INIT_PRESETS_STATE,
+  INIT_WORKSPACE_STATE
+}
