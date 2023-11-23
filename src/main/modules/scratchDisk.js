70c9f7b243572d8646afc9b7ea03a8eaadf31710
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-23 00:55:32 -0500

    moved value of vairable scratchDisk to return object of new factory function createScratchDisk. scratchDisk now exports as instance of createScratchDisk factory. moved functions clearFiles and clearFileByAge into scope of function createScratchDisk and added underscores to their names to indicate private methods.

--- src/main/modules/scratchDisk.js
+++ src/main/modules/scratchDisk.js
@@ -3,84 +3,88 @@ import path from 'path'
 
 import { PREFERENCES_PATH } from './constants'
 
-const clearFiles = async (dir, id) => {
-	let files = await fsp.readdir(dir)
-
-	if (!files.length) return Promise.resolve()
-
-	if (id) files = files.filter(file => file.startsWith(id))
-
-	return Promise.all(files.map(file => fsp.unlink(path.join(dir, file))))
-}
-
-const clearFilesByAge = async (dir, age) => {
-	const files = await fsp.readdir(dir)
-	const now = Date.now()
-
-	return Promise.all(files.map(file => (async () => {
-		const filePath = path.join(dir, file)
-		const { ctime } = await fsp.lstat(filePath)
+const createScratchDisk = () => {
+	const _clearFiles = async (dir, id) => {
+		let files = await fsp.readdir(dir)
 	
-		if (now - ctime > age) {
-			return fsp.unlink(filePath)
-		} else {
-			return Promise.resolve()
-		}
-	})()))
-}
+		if (!files.length) return Promise.resolve()
+	
+		if (id) files = files.filter(file => file.startsWith(id))
+	
+		return Promise.all(files.map(file => fsp.unlink(path.join(dir, file))))
+	}
+	
+	const _clearFilesByAge = async (dir, age) => {
+		const files = await fsp.readdir(dir)
+		const now = Date.now()
+	
+		return Promise.all(files.map(file => (async () => {
+			const filePath = path.join(dir, file)
+			const { ctime } = await fsp.lstat(filePath)
+		
+			if (now - ctime > age) {
+				return fsp.unlink(filePath)
+			} else {
+				return Promise.resolve()
+			}
+		})()))
+	}
 
-export const scratchDisk = {
-	imports: {
-		path: '',
-		clear(id) {
-			return clearFiles(this.path, id)
+	return {
+		imports: {
+			path: '',
+			clear(id) {
+				return _clearFiles(this.path, id)
+			},
+			clearByAge(age = 144e4) {
+				return _clearFilesByAge(this.path, age)
+			}
 		},
-		clearByAge() {
-			return clearFilesByAge(this.path, 144e4)
+		exports: {
+			path: '',
+			clear(id) {
+				return _clearFiles(this.path, id)
+			}
+		},
+		previews: {
+			path: '',
+			clear(id) {
+				return _clearFiles(this.path, id)
+			}
+		},
+		clearAllByAge() {
+			return Promise.all([
+				this.imports.clearByAge(),
+				this.exports.clear(),
+				this.previews.clear()
+			])
+		},
+		clearAll() {
+			return Promise.all([
+				this.imports.clear(),
+				this.exports.clear(),
+				this.previews.clear()
+			])
+		},
+		async init() {
+			await this.update()
+			await this.clearAllByAge() 
+		},
+		async update() {
+			const prefs = JSON.parse(await fsp.readFile(PREFERENCES_PATH))
+			const opts = { recursive: true }
+		
+			this.imports.path = path.join(prefs.scratchDisk.imports, 'able2_imports')
+			this.exports.path = path.join(prefs.scratchDisk.exports, 'able2_exports')
+			this.previews.path = path.join(prefs.scratchDisk.previews, 'able2_previews')
+		
+			return Promise.all([
+				fsp.mkdir(this.imports.path, opts),
+				fsp.mkdir(this.exports.path, opts),
+				fsp.mkdir(this.previews.path, opts)
+			])
 		}
-	},
-	exports: {
-		path: '',
-		clear(id) {
-			return clearFiles(this.path, id)
-		}
-	},
-	previews: {
-		path: '',
-		clear(id) {
-			return clearFiles(this.path, id)
-		}
-	},
-	clearAllByAge() {
-		return Promise.all([
-			this.imports.clearByAge(),
-			this.exports.clear(),
-			this.previews.clear()
-		])
-	},
-	clearAll() {
-		return Promise.all([
-			this.imports.clear(),
-			this.exports.clear(),
-			this.previews.clear()
-		])
-	},
-	async init() {
-		await this.update()
-		await this.clearAllByAge() 
-	},
-	async update() {
-		const prefs = JSON.parse(await fsp.readFile(PREFERENCES_PATH))
-		const opts = { recursive: true }
-	
-		this.imports.path = path.join(prefs.scratchDisk.imports, 'able2_imports')
-		this.exports.path = path.join(prefs.scratchDisk.exports, 'able2_exports')
-		this.previews.path = path.join(prefs.scratchDisk.previews, 'able2_previews')
-	
-		return Promise.all([
-			fsp.mkdir(this.imports.path, opts),
-			fsp.mkdir(this.exports.path, opts),
-			fsp.mkdir(this.previews.path, opts)
-		])
 	}
 }
+
+export const scratchDisk = createScratchDisk()
