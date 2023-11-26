1f9775f7e7d3bef7cc48fc2e55031948c0379c2a
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-11-26 11:59:54 -0500

    added local state workspaceLoaded to track when saved data is available

--- src/renderer/store/workspace.js
+++ src/renderer/store/workspace.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { arrayOf, element, oneOfType } from 'prop-types'
 
 import { workspaceReducer as reducer } from 'reducer'
@@ -14,6 +14,7 @@ export const WorkspaceContext = createContext()
 export const WorkspaceProvider = ({ children }) => {
 	const [ state, dispatch ] = useAugmentedDispatch(reducer, INIT_WORKSPACE_STATE)
 	const { preferences: { aspectRatioMarkers }, prefsLoaded } = useContext(PrefsContext)
+	const [ workspaceLoaded, setWorkspaceLoaded ] = useState(false)
 
 	useEffect(() => {
 		if (!prefsLoaded) return
@@ -23,12 +24,15 @@ export const WorkspaceProvider = ({ children }) => {
 				...await interop.requestWorkspace(),
 				aspectRatioMarkers
 			}))
+
+			setWorkspaceLoaded(true)
 		})()
 	}, [prefsLoaded, aspectRatioMarkers])
 
 	return (
 		<WorkspaceContext.Provider value={{
 			...state,
+			workspaceLoaded,
 			dispatch
 		}}>
 			{ children }
