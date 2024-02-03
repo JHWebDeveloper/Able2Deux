c4219b218262c7bf6060fa242b315194337aceb5
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2024-02-02 21:12:22 -0500

    removed function getOverlayInnerDimensions

--- src/main/modules/utilities/index.js
+++ src/main/modules/utilities/index.js
@@ -27,23 +27,6 @@ export const fileExistsPromise = async fileOrDir => {
 	}
 }
 
-export const getOverlayInnerDimensions = (size, overlay) => {
-	const is1080 = size === '1080'
-
-	return {
-		tv: {
-			width: is1080 ? 1576 : 1050,
-			height: is1080 ? 886 : 590,
-			y: is1080 ? 78 : 52
-		},
-		laptop: {
-			width: is1080 ? 1366 : 912,
-			height: is1080 ? 778 : 518,
-			y: is1080 ? 86 : 58
-		}
-	}[overlay]
-}
-
 const isObject = val => !!val && typeof val === 'object' && val.constructor === Object
 
 export const innerMergeObjectKeys = (objL, objR) => {
